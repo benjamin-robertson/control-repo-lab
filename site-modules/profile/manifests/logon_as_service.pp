67b1b6c1f32bf283162dbe2d07516bb0f16aebdc
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-15 11:22:50 +1100

    update

--- site-modules/profile/manifests/logon_as_service.pp
+++ site-modules/profile/manifests/logon_as_service.pp
@@ -2,7 +2,7 @@ class profile::logon_as_service {
 
   local_security_policy { 'Log on as a service':
     ensure => 'present',
-    policy_value => ['ben','cloudbase-init','NT SERVICE\ALL SERVICES']
+    policy_value => ['cloudbase-init','ben','NT SERVICE\ALL SERVICES']
   }
 
 }
