ed04db117cac3aed2e40dbfff31b053d11ff915f
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-16 16:44:42 +1100

    Final reg change

--- site-modules/profile/manifests/registry.pp
+++ site-modules/profile/manifests/registry.pp
@@ -1,10 +1,10 @@
 class profile::registry {
-  registry_value { 'HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\IEHarden':
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => 'present',
     type => 'dword',
     data => '1',
   }
-  registry_value { 'HKEY_LOCAL_MACHINE\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\IEHarden':
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => 'present',
     type => 'dword',
     data => '1',
