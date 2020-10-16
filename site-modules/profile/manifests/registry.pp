3a98beb4443e695396f1975cd393075136e68ded
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-16 15:29:20 +1100

    test

--- site-modules/profile/manifests/registry.pp
+++ site-modules/profile/manifests/registry.pp
@@ -1,6 +1,7 @@
 class profile::registry {
-  registry_value { 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\poodle':
+  registry_value { 'poodle':
     ensure => 'present',
+    path => 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap',
     type => 'dword',
     data => '1234',
   }
