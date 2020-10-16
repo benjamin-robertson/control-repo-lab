9b5956919eb9440dda51b00fac8cfe3187273266
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-16 15:40:08 +1100

    Harden IE

--- site-modules/profile/manifests/registry.pp
+++ site-modules/profile/manifests/registry.pp
@@ -1,7 +1,12 @@
 class profile::registry {
-  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\poodle':
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\IEHardenIENoWarn':
     ensure => 'present',
     type => 'dword',
-    data => '1234',
+    data => '1',
+  }
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\IEHarden':
+    ensure => 'present',
+    type => 'dword',
+    data => '1',
   }
 }
