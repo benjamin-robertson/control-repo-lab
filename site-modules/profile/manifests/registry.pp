1d0c2fd811c3a33910d553c17426ba89fd35f732
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-16 15:56:39 +1100

    ESC try again

--- site-modules/profile/manifests/registry.pp
+++ site-modules/profile/manifests/registry.pp
@@ -1,10 +1,10 @@
 class profile::registry {
-  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\IEHardenIENoWarn':
+  registry_value { 'HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\IEHarden':
     ensure => 'present',
     type => 'dword',
     data => '1',
   }
-  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\IEHarden':
+  registry_value { 'HKEY_LOCAL_MACHINE\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\IEHarden':
     ensure => 'present',
     type => 'dword',
     data => '1',
