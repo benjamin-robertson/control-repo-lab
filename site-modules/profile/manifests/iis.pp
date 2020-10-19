7c76529d28dfb1fc940347573e2f3f3751a08491
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-19 14:29:29 +1100

    upload web page

--- site-modules/profile/manifests/iis.pp
+++ site-modules/profile/manifests/iis.pp
@@ -1,16 +1,18 @@
 class profile::iis {
 
+$website = "<HTML><HEAD><TITLE>Ben's super really cool website</TITLE></HEAD><BODY><h1>Welcome to ben's pretty cool website</h1><br/><p>This website is hosted on IIS, pretty basic, however its been configured and all setup via Puppet. No manual intervention required, repeatable and automated :) </p></BODY></HTML>"
+
 # install IIS feature
   dsc_windowsfeature {'IIS':
-    ensure => 'present',
+    ensure   => 'present',
     dsc_name => 'Web-Server',
   }
   dsc_windowsfeature {'IIS-tools':
-    ensure => 'present',
+    ensure   => 'present',
     dsc_name => 'Web-Mgmt-Tools',
   }
     dsc_windowsfeature {'IIS-tools-scripts':
-    ensure => 'present',
+    ensure   => 'present',
     dsc_name => 'Web-Scripting-Tools',
   }
 # remove default website
@@ -18,5 +20,18 @@ class profile::iis {
     ensure  => absent,
     #require => Iis_feature['Web-WebServer'],
   }
-
+  # create folder and html page to serve
+  file {'c:\inetpub\bensite':
+    ensure => 'directory',
+    owner  => 'system',
+  }
+  file {'c:\inetpub\bensite\index.html':
+    ensure  => 'file',
+    content => $website,
+  }
+  acl { 'c:\inetpub\bensite':
+      permissions => [
+        { identity => 'IIS_IUSRS', rights => ['read','execute'] },
+      ],
+    }
 }
