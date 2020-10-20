bfcc150ab0ce8c8f977a140b224eaa6554c0d958
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-20 14:10:55 +1100

    reboot after install

--- site-modules/profile/manifests/choco.pp
+++ site-modules/profile/manifests/choco.pp
@@ -1,11 +1,15 @@
 class profile::choco {
   include chocolatey
   package { '7zip.install':
-    ensure   => 'absent',
+    ensure   => 'latest',
     provider => 'chocolatey',
   }
   package { 'vlc':
     ensure   => 'latest',
     provider => 'chocolatey',
+  }
+  #Reboot after installation of 7zip
+  reboot { 'afterInstall':
+    subscribe  => package['7zip.install'],
   }
 }
