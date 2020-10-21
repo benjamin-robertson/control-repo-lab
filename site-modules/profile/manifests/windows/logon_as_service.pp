0fce720f1ba6dcc4f99d11e84bd3bd760ef97f0f
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-22 10:55:45 +1100

    comments

--- site-modules/profile/manifests/windows/logon_as_service.pp
+++ site-modules/profile/manifests/windows/logon_as_service.pp
@@ -1,13 +1,14 @@
 class profile::windows::logon_as_service
 ( String $logon_as_service_accounts = 'ben,cloudbase-init,NT SERVICE\ALL SERVICES' )
 {
-
   # TODO: What if you wanted to extend this profile to allow certain nodes to have
   #  different lists of users log on as a service. A good example could be that a
   # certain application requires that "appuer" can log on as a service, but not all
   # servers have this application. How could you handle that?
   #
   # Still neeed this ^^
+  # 
+  # Ben: Are you after something like this? using a paramater which can be changed when the class is called?
 
   local_security_policy { 'Log on as a service':
     ensure       => 'present',
