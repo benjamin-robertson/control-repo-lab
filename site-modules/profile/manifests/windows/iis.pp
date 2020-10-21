042f8c8a865e979ed670ec6829d18c0898b8f2fb
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-22 10:28:13 +1100

    commit

--- site-modules/profile/manifests/windows/iis.pp
+++ site-modules/profile/manifests/windows/iis.pp
@@ -30,7 +30,7 @@ class profile::windows::iis {
   # content => template('profile/index.epp'),
   file {'c:\inetpub\bensite\index.html':
     ensure => 'file',
-    source => 'puppet:///modules/profile/files/index.html',
+    source => 'puppet:///modules/profile/index.html',
   }
   # Set permissions on the folder so IIS service acccount can read the file
   acl { 'c:\inetpub\bensite':
