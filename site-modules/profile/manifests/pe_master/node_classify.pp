3b00d5ba6be2c1816f92058545456d735bcdd2d7
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-22 14:35:25 +1100

    Apply role to class

--- site-modules/profile/manifests/pe_master/node_classify.pp
+++ site-modules/profile/manifests/pe_master/node_classify.pp
@@ -2,7 +2,8 @@ class profile::pe_master::node_classify {
   node_group { 'Windows Hosts - auto':
     ensure      => 'present',
     environment => 'production',
-    rule       => ['and', ['=', ['fact', 'osfamily'], 'windows']],
+    rule        => ['and', ['=', ['fact', 'osfamily'], 'windows']],
     parent      => 'All Nodes',
+    classes     => {'role::windows_server' => {}},
   }
 }
