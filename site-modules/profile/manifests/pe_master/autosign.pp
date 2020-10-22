3915975626564cd47fa56d1c5e79d09ace73ed66
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-22 13:02:00 +1100

    syntax error

--- site-modules/profile/manifests/pe_master/autosign.pp
+++ site-modules/profile/manifests/pe_master/autosign.pp
@@ -1,6 +1,6 @@
 class profile::pe_master::autosign{
   autosign { 'manage_autosign':
     ensure => 'latest',
-    config => {general => {loglevel => 'INFO'},
+    config => {'general' => {'loglevel' => 'INFO'},
   }
 }
