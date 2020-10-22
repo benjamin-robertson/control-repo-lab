adc98ea1ab9f94921b27890318434209720dabd0
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-22 14:31:06 +1100

    error

--- site-modules/profile/manifests/pe_master/node_classify.pp
+++ site-modules/profile/manifests/pe_master/node_classify.pp
@@ -2,7 +2,7 @@ class profile::pe_master::node_classify {
   node_group { 'Windows Hosts - auto':
     ensure      => 'present',
     environment => 'production',
-    rules       => ['and', ['=', ['fact', 'osfamily'], 'windows']],
+    rule       => ['and', ['=', ['fact', 'osfamily'], 'windows']],
     parent      => 'All Nodes',
   }
 }
