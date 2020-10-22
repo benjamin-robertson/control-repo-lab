07bc15532980d9c873a13483806e1dfc026ab9aa
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-22 14:41:13 +1100

    Linux hosts

--- site-modules/profile/manifests/pe_master/node_classify.pp
+++ site-modules/profile/manifests/pe_master/node_classify.pp
@@ -6,4 +6,11 @@ class profile::pe_master::node_classify {
     parent      => 'All Nodes',
     classes     => {'role::windows_server' => {}},
   }
+  node_group { 'Linux host - auto':
+    ensure      => 'present',
+    environment => 'production',
+    rule        => ['and', ['=', ['fact', 'osfamily'], 'RedHat'], ['!~', ['fact', 'pe_major_version'], '']],
+    parent      => 'All Nodes',
+    #classes     => {'role::windows_server' => {}},
+  }
 }
