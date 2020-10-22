474ac11ada99f2553d925f53b69a6ea1d3c4bae1
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-22 14:29:41 +1100

    with rule

--- site-modules/profile/manifests/pe_master/node_classify.pp
+++ site-modules/profile/manifests/pe_master/node_classify.pp
@@ -2,6 +2,7 @@ class profile::pe_master::node_classify {
   node_group { 'Windows Hosts - auto':
     ensure      => 'present',
     environment => 'production',
+    rules       => ['and', ['=', ['fact', 'osfamily'], 'windows']],
     parent      => 'All Nodes',
   }
 }
