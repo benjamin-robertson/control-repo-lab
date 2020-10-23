c1d7633ca2e184b31cf05d71427bdbc1b1b8bc5c
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-23 14:19:59 +1100

    syntax error

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,7 +12,7 @@
 # Disable filebucket by default for all File resources:
 # https://github.com/puppetlabs/docs-archive/blob/master/pe/2015.3/release_notes.markdown#filebucket-resource-no-longer-created-by-default
 File {
-  backup  => false
+  backup  => false,
   content => 'Set in site.pp under top scope',
 }
 ## Node Definitions ##
