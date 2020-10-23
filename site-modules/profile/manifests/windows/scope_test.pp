b68a08ab736eff985bae219e129b8de3d67425c0
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-10-23 14:09:23 +1100

    commit

--- site-modules/profile/manifests/windows/scope_test.pp
+++ site-modules/profile/manifests/windows/scope_test.pp
@@ -1,8 +1,5 @@
 class profile::windows::scope_test {
   notify{"Varible Ben = ${ben}":}
-  File {
-    content => 'From resource default in this class',
-  }
   file { 'text.txt':
     path   => 'c:\temp\test.txt',
     ensure => 'present',
