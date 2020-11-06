bcd5f852a06bd77e2f7a48f5c6ebf540ab83d911
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-11-06 11:54:18 +1100

    update site pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,9 +11,10 @@
 
 # Disable filebucket by default for all File resources:
 # https://github.com/puppetlabs/docs-archive/blob/master/pe/2015.3/release_notes.markdown#filebucket-resource-no-longer-created-by-default
+
 File {
   backup  => false,
-  content => 'Set in site.pp under top scope',
+#  content => 'Set in site.pp under top scope',
 }
 ## Node Definitions ##
 
