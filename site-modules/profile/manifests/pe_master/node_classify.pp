b9e804418321da52858e8b447ee74775ed411334
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-11-12 18:06:12 +1100

    Update enfironemnt group

--- site-modules/profile/manifests/pe_master/node_classify.pp
+++ site-modules/profile/manifests/pe_master/node_classify.pp
@@ -17,7 +17,7 @@ class profile::pe_master::node_classify {
     ensure               => 'present',
     environment          => 'development',
     rule                 => ['and', ['~', 'name', 'dev']],
-    parent               => 'All Nodes',
+    parent               => 'All Environments',
     override_environment => 'true',
   }
 }
