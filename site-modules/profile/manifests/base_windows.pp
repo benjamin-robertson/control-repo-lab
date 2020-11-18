e3034e3f6c0e7d0047ca672a266f593afc5436c0
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-11-18 18:11:48 +1100

    enabling motd with basic message

--- site-modules/profile/manifests/base_windows.pp
+++ site-modules/profile/manifests/base_windows.pp
@@ -23,4 +23,5 @@ class profile::base_windows {
   include profile::windows::scope_test
   include profile::windows::hosts
   include profile::windows::vscode
+  include profile::windows::motdwindows
 }
---
+++ site-modules/profile/manifests/windows/motdwindows.pp
@@ -0,0 +1,3 @@
+class profile::windows::motdwindows {
+  include motd
+}
