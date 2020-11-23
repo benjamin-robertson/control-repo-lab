a8daa8e8a0d68651b0dfec330c81306f01f8ad48
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-11-23 14:13:36 +1100

    using variable

--- site-modules/profile/manifests/windows/motdwindows.pp
+++ site-modules/profile/manifests/windows/motdwindows.pp
@@ -2,6 +2,6 @@ class profile::windows::motdwindows (
   String $content = undef,
 ) {
   class { 'motd':
-    content => 'Welcome to windows resource like class defintion',
+    content => $content,
   }
 }
