9314c22967928c39fbf9619c0a77c1d40ad1aa07
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-12-08 10:19:40 +1100

    motd

--- site-modules/profile/manifests/linux/motd_linux.pp
+++ site-modules/profile/manifests/linux/motd_linux.pp
@@ -2,6 +2,11 @@
 #
 class profile::linux::motd_linux {
   Class { 'motd':
-    content => 'Welcome to the linux system motd message, enjoy!!!',
+    content => @(EOT)
+    Welcome to the linux system motd message, enjoy!!!
+    This is been placed over many lines
+    Lines are good
+    It's easier to read :)
+    | EOT
   }
 }
