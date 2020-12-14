9f83a212b8919c2375f8451609ea082152e6b18f
Benjamin Robertson <benjamin@Benjamins-MacBook-Pro.local>
2020-12-14 12:22:55 +1100

    Testing code

--- site-modules/profile/manifests/linux/motd_linux.pp
+++ site-modules/profile/manifests/linux/motd_linux.pp
@@ -1,14 +1,8 @@
 # Class: profile::linux::m
 #
 class profile::linux::motd_linux {
+  $motdcontent = lookup(motd_linux)
   Class { 'motd':
-    content => @(EOT)
-    Welcome to the linux system motd message, enjoy!!!
-    This is been placed over many lines
-    Lines are good
-    It's easier to read :)
-    Adding extra stuff to test compiler
-    Upgraded to 2019.8.4
-    | EOT
+    content => $motdcontent,
   }
 }
