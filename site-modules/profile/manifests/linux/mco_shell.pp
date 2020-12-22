6152726f95315d2138cb70d1f95c2c16cf7fceed
Benjamin Robertson <benrobertson9876@gmail.com>
2020-12-22 13:06:26 +1100

    shell mco

---
+++ site-modules/profile/manifests/linux/mco_shell.pp
@@ -0,0 +1,18 @@
+# Class: mco_shell
+#
+#
+class mco_shell (
+  String $mco_location = "/opt/puppetlabs/mcollective/plugins/mcollective/agent/",
+  Array $mco_files = ['shell.rb','shell.ddl','/shell/job.rb'],
+) {
+  # iterate over the array of files to manage
+  $mco_files.each | String $files| {
+    file {"${mco_location}${files}":
+      ensure => present,
+      mode   => '0644',
+      owner  => 'root',
+      group  => 'root',
+      source => "puppet:///modules/profile/${files}",
+    }
+  }
+}
