84598abaa6489547c4ea7809ec93ec01ef6009bf
Benjamin Robertson <benrobertson9876@gmail.com>
2021-01-15 12:21:43 +1100

    test

--- site-modules/profile/manifests/base_observium_offline.pp
+++ site-modules/profile/manifests/base_observium_offline.pp
@@ -2,8 +2,20 @@
 #
 class profile::base_observium_offline {
   # Include observium module class
+$my_repo = { 'epel' => {
+    'ensure'   => 'present',
+    'enabled'  => '1',
+    'descr'    => 'Extra packages for enterprise linux',
+    'baseurl'  => 'http://myrepo.local/epel7',
+    'gpgcheck' => '1',
+    'gpgkey'   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-%{facts.os.release.major}',
+    'target'   => '/etc/yum.repos.d/epel.repo',
+    },
+}
+
 class { 'observium':
-    manage_fw     => true,
-    manage_apache => true,
+    repos          => $my_repo,
+    download_url   => '/tmp/',
+    installer_name => 'observium-community-latest.tar.gz',
 }
 }
