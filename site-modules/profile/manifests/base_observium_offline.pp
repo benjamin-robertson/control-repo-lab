a41aa9f5c85e0df2bd5b76b01ffde0bb59996324
Benjamin Robertson <benrobertson9876@gmail.com>
2021-01-15 12:18:03 +1100

    manage apahce

--- site-modules/profile/manifests/base_observium_offline.pp
+++ site-modules/profile/manifests/base_observium_offline.pp
@@ -4,6 +4,6 @@ class profile::base_observium_offline {
   # Include observium module class
 class { 'observium':
     manage_fw     => false,
-    manage_apache => false,
+    manage_apache => true,
 }
 }
