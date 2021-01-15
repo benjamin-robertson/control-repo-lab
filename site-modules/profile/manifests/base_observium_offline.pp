5aa7c50dbb96a654e9b1a341b2e9fa3a02646f8c
Benjamin Robertson <benrobertson9876@gmail.com>
2021-01-15 12:14:35 +1100

    commit

--- site-modules/profile/manifests/base_observium_offline.pp
+++ site-modules/profile/manifests/base_observium_offline.pp
@@ -3,8 +3,7 @@
 class profile::base_observium_offline {
   # Include observium module class
 class { 'observium':
-    manage_ssl      => true,
-    #custom_ssl_cert => '/opt/observium/ssl/cert.pem',
-    #custom_ssl_key  => '/opt/observium/ssl/key.pem',
+    manage_fw     => false,
+    manage_apache => false,
 }
 }
