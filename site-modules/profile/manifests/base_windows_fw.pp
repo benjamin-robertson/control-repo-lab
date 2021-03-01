c88dcd9387521d91e039847ecbc65de05a39c2ea
Benjamin Robertson <ben.robertson@puppet.com>
2021-03-01 13:35:13 +1100

    added fw rule

--- site-modules/profile/manifests/base_windows_fw.pp
+++ site-modules/profile/manifests/base_windows_fw.pp
@@ -2,6 +2,18 @@ class profile::base_windows_fw {
   class { 'windows_firewall':
     ensure => 'running',
   }
+
+  windows_firewall::exception { 'WINRM':
+    ensure       => present,
+    direction    => 'in',
+    action       => 'allow',
+    enabled      => true,
+    protocol     => 'TCP',
+    local_port   => '80',
+    remote_port  => 'any',
+    display_name => 'HTTP in - puppet',
+    description  => 'Inbound rule for HTTP - puppet',
+  }
 #  windows_firewall_rule { "puppet - all icmpv4":
 #    ensure    => present,
 #    direction => 'inbound',
