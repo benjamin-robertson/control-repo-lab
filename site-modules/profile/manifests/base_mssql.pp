07a81b8a0bd83359defd1bbd6d7675e219ac4b10
Benjamin Robertson <ben.robertson@puppet.com>
2021-03-30 15:47:00 +1100

    Changed mssql install user

--- site-modules/profile/manifests/base_mssql.pp
+++ site-modules/profile/manifests/base_mssql.pp
@@ -5,6 +5,6 @@ class profile::base_mssql {
   sqlserver_instance{ 'BenSQL':
     features              => ['SQL'],
     source                => 'C:/iso/',
-    sql_sysadmin_accounts => ['myuser'],
+    sql_sysadmin_accounts => ['Administrator'],
   }
 }
