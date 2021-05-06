73d506cbfe7777acb490ec8a26d22c16913badc8
Benjamin Robertson <ben.robertson@puppet.com>
2021-05-06 11:39:34 +1000

    update

--- site-modules/bolt/plans/first_patch.pp
+++ site-modules/bolt/plans/first_patch.pp
@@ -2,7 +2,7 @@ plan bolt::first_patch (
   String $patch_group,
 ) {
 
-  $patch_results = run_task('pe_patch::last_boot_time_nix', 'PE_first_patch_run')
+  $patch_results = run_task('pe_patch::last_boot_time_nix', $patch_group)
 
   return({
     'patch_results' => $patch_results,
