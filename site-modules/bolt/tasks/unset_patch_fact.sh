3029e6ad2533f06b6d624401e6c782c74880df81
Benjamin Robertson <ben.robertson@puppet.com>
2021-05-06 17:24:39 +1000

    test

--- site-modules/bolt/tasks/unset_patch_fact.sh
+++ site-modules/bolt/tasks/unset_patch_fact.sh
@@ -2,7 +2,7 @@
 
 factfile=$PT_factfile
 factname=$PT_factname
-
+echo $PT_factfile
 echo sed -i "s/$factname=true/$factname=false/g" $factfile
 
 sed -i "s/$factname=true/$factname=false/g" $factfile
