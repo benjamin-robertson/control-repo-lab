6c837d33e856f116a74ee732b79e6c53c218fb43
Benjamin Robertson <ben.robertson@puppet.com>
2021-05-06 16:25:38 +1000

    fix

--- site-modules/bolt/plans/first_patch.pp
+++ site-modules/bolt/plans/first_patch.pp
@@ -2,7 +2,7 @@ plan bolt::first_patch (
   String $patch_group,
 ) {
 
-  $nodes_to_patch = puppetdb_query("inventory[certname] { facts.patchme = "true" }")
+  $nodes_to_patch = puppetdb_query("inventory[certname] { facts.patchme = \"true\" }")
 
   $patch_results = run_task('pe_patch::last_boot_time_nix', $nodes_to_patch)
 
