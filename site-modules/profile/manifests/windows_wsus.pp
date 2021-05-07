cab0a9434f71a00abeb1be700fa21f59e8fee865
Benjamin Robertson <ben.robertson@puppet.com>
2021-05-07 15:01:39 +1000

    fix

--- site-modules/profile/manifests/windows_wsus.pp
+++ site-modules/profile/manifests/windows_wsus.pp
@@ -3,7 +3,7 @@
 #
 class profile::windows_wsus {
   class { 'wsus_client':
-    erver_url            => 'http://ip-172-31-14-59.ap-southeast-2.compute.internal:8530',
+    server_url           => 'http://ip-172-31-14-59.ap-southeast-2.compute.internal:8530',
     enable_status_server => true,
   }
 }
