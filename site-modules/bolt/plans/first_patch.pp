a42da21e87f3d106c7af45a098bc3866688d5655
Benjamin Robertson <ben.robertson@puppet.com>
2021-05-07 10:47:09 +1000

    updated plan

--- site-modules/bolt/plans/first_patch.pp
+++ site-modules/bolt/plans/first_patch.pp
@@ -1,5 +1,5 @@
 plan bolt::first_patch (
-  String $patch_group,
+  String $patch_fact,
 ) {
 
   $nodes_to_patch = puppetdb_query("inventory[certname] { facts.patchme = \"true\" }")
@@ -8,17 +8,25 @@ plan bolt::first_patch (
   $filtered_nodes = $nodes_to_patch.map | $i | { $i['certname']}
   $targets = get_targets($filtered_nodes)
 
-  $patch_results = run_task('initial_patch::unset_patch_fact',
+  $unset_fact_result = run_task('initial_patch::unset_patch_fact',
                             $targets,
                             factfile => '/opt/puppetlabs/facter/facts.d/patchme.txt',
                             factname => 'patchme',
                   )
 
+  # Run the patch job
+  $to_patch = un_task('pe_patch::patch_server',
+                              $targets,
+                              reboot          => 'patched',
+                              security_only   => 'false',
+                              '_catch_errors' => true
+                    )
 
   return({
     'patch_results'  => $patch_results,
     'nodes_to_patch' => $nodes_to_patch,
     'filtered_nodes' => $filtered_nodes,
     'targets'        => $targets,
+    'to_patch'       => $to_patch,
   })
 }
