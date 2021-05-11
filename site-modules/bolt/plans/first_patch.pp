3eb752cb93f23b62e373143541d28164a09aafd5
Benjamin Robertson <ben.robertson@puppet.com>
2021-05-11 10:03:44 +1000

    asdf

--- site-modules/bolt/plans/first_patch.pp
+++ site-modules/bolt/plans/first_patch.pp
@@ -56,7 +56,7 @@ plan bolt::first_patch (
 
   out::message("resullts from to_patch : ${nodes_to_unset_fact}")
 
-  $to_patch.each | $da | { $da.each | $in | { out::message("results: ${in}") } }
+  $to_patch.each | $da | { $da.each | $in | { out::message("resultssss: ${in}") } }
 
   #$unset_fact_result = run_task('initial_patch::unset_patch_fact',
   #                          $nodes_to_unset_fact,
