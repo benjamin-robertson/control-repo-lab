cbe185807db47484b158bc248c1c9a13a4c185a7
Benjamin Robertson <ben.robertson@puppet.com>
2021-05-13 12:06:19 +1000

    Added abide

---
+++ site-modules/profile/manifests/base_hardened_windows.pp
@@ -0,0 +1,6 @@
+# Class: profile::base_hardened_windows
+#
+#
+class profile::base_hardened_windows {
+  include abide_windows
+}
---
+++ site-modules/role/manifests/hardened_windows.pp
@@ -0,0 +1,6 @@
+# Class: name
+#
+#
+class role::hardened_windows {
+  include profile::base_hardened_windows
+}
