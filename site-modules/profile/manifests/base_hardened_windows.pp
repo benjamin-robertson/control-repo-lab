cdf7eaa42a237ed1eac78f350318ec77f37fb37d
Benjamin Robertson <ben.robertson@puppet.com>
2021-06-23 09:19:26 +1000

    Updated registry

--- site-modules/profile/manifests/base_hardened_windows.pp
+++ site-modules/profile/manifests/base_hardened_windows.pp
@@ -4,10 +4,17 @@
 class profile::base_hardened_windows {
   include abide_windows
 
-  registry::value {'HKU\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\LaunchTo':
-    key   => 'HKU\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced',
+  registry::value {'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\LaunchTo':
+    key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced',
     value => 'LaunchTo',
     type  => 'dword',
     data  => 1,
   }
+
+  #registry::value {'HKU\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\LaunchTo':
+  #  key   => 'HKU\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced',
+  #  value => 'LaunchTo',
+  #  type  => 'dword',
+  #  data  => 1,
+  #}
 }
