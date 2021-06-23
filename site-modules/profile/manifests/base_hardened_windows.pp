78f391cc2360a5121ad741b307abdbaccc49881d
Benjamin Robertson <ben.robertson@puppet.com>
2021-06-23 10:04:22 +1000

    File extension

--- site-modules/profile/manifests/base_hardened_windows.pp
+++ site-modules/profile/manifests/base_hardened_windows.pp
@@ -4,11 +4,20 @@
 class profile::base_hardened_windows {
   include abide_windows
 
+  # launch to pc not quick access
   registry::value {'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\LaunchTo':
     key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced',
     value => 'LaunchTo',
     type  => 'dword',
     data  => 1,
+  }
+
+  # show file extension
+  registry::value {'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\HideFileExt':
+    key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced',
+    value => 'HideFileExt',
+    type  => 'dword',
+    data  => 0,
   }
 
   #registry::value {'HKU\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\LaunchTo':
