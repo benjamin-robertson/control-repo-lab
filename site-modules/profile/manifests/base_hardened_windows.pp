001c5444793f0a77042581ee54d7c660bca41466
Benjamin Robertson <ben.robertson@puppet.com>
2021-06-28 13:20:52 +1000

    Restored abide under hardened windows

--- site-modules/profile/manifests/base_hardened_windows.pp
+++ site-modules/profile/manifests/base_hardened_windows.pp
@@ -2,37 +2,5 @@
 #
 #
 class profile::base_hardened_windows {
-  #include abide_windows
-
-  # launch to pc not quick access
-  registry::value {'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\LaunchTo':
-    key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced',
-    value => 'LaunchTo',
-    type  => 'dword',
-    data  => 1,
-  }
-
-  # show file extension
-  registry::value {'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\HideFileExt':
-    key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced',
-    value => 'HideFileExt',
-    type  => 'dword',
-    data  => 0,
-  }
-
-  # Rename ethernet to public 1
-  keys($facts['networking']['interfaces']).each | Integer $index, String $interface | {
-    $finalindex = $index + 1
-    dsc_netadaptername { $interface:
-      dsc_name    => $interface,
-      dsc_newname => "hello ${finalindex}",
-    }
-  }
-
-  #registry::value {'HKU\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\LaunchTo':
-  #  key   => 'HKU\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced',
-  #  value => 'LaunchTo',
-  #  type  => 'dword',
-  #  data  => 1,
-  #}
+  include abide_windows
 }
