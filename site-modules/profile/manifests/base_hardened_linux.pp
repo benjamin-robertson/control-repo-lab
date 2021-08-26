20909781eaf421e54e45b966ca77f36a01800d2a
Benjamin Robertson <ben.robertson@puppet.com>
2021-08-26 15:11:27 +1000

    asdf

--- site-modules/profile/manifests/base_hardened_linux.pp
+++ site-modules/profile/manifests/base_hardened_linux.pp
@@ -2,5 +2,25 @@
 #
 #
 class profile::base_hardened_linux {
-  include cem_linux
+  #include cem_linux
+    $timeservers = { 'server 1.1.1.1' => 'iburst minpoll 4 maxpoll 4'}
+    Augeas {
+      context => '/files/etc/chrony.conf'
+    }
+
+    augeas { "chrony_d_agues":
+      changes => 'set server[1] 1.1.1.1 iburst minpoll 4 maxpoll 4',
+    }
+
+    #$_parts = split($timeservers[0], /\s+/)
+    #augeas { "chrony_${_parts[0]}_${_parts[1]}":
+    #  changes => "set ${_parts[0]}[1] ${_parts[1]}",
+    #}
+    #if size($_parts) > 2 {
+    #  $_parts[2, -1].each | String $prt | {
+    ##    augeas { "chrony_${_parts[0]}_${_parts[1]}_${prt}":
+    #     changes => "touch ${_parts[0]}[1]/${prt}",
+    #   }
+    # }
+  # }
 }
