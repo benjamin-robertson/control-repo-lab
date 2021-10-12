fced8af181e1346b33014bc8ffd4392090df03fd
Benjamin Robertson <ben.robertson@puppet.com>
2021-10-13 10:38:47 +1100

    Update folder location

--- site-modules/profile/manifests/base_chocolatey.pp
+++ site-modules/profile/manifests/base_chocolatey.pp
@@ -2,7 +2,7 @@
 #
 #
 class profile::base_chocolatey (
-  String $chocolately_package_source = 'http://ip-172-31-4-194.ap-southeast-2.compute.internal/',
+  String $chocolately_package_source = 'http://ip-172-31-4-194.ap-southeast-2.compute.internal/choco',
   String $chocolatey_download_url = 'http://ip-172-31-4-194.ap-southeast-2.compute.internal/chocolatey.0.11.2.nupkg',
 ) {
 
