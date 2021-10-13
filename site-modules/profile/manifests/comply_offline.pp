f6dae4b3c8e09a908aba64f198281102cf1c4d75
Benjamin Robertson <ben.robertson@puppet.com>
2021-10-14 10:54:14 +1100

    invalid checksum

--- site-modules/profile/manifests/comply_offline.pp
+++ site-modules/profile/manifests/comply_offline.pp
@@ -41,10 +41,11 @@ class profile::comply_offline (
 
   # Sync java msi locally
   file { $java_msi_name:
-    ensure  => file,
-    source  => "${choco_http_source}${java_msi_name}",
-    path    => "${chocolately_package_dest}\\${java_msi_name}",
-    require => File['choco_packages_dir'],
+    ensure   => file,
+    checksum => '4365EA6D753CE61CE809FCD94E6CDA1723673B3A79B4CA71A01599EB0AECEF0I',
+    source   => "${choco_http_source}${java_msi_name}",
+    path     => "${chocolately_package_dest}\\${java_msi_name}",
+    require  => File['choco_packages_dir'],
   }
 
   # Install java
