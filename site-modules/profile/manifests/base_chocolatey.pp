dad655c5c9a30544c915d1264c45437f1db00be6
Benjamin Robertson <ben.robertson@puppet.com>
2021-10-13 11:36:03 +1100

    Create local packges

--- site-modules/profile/manifests/base_chocolatey.pp
+++ site-modules/profile/manifests/base_chocolatey.pp
@@ -2,8 +2,10 @@
 #
 #
 class profile::base_chocolatey (
-  String $chocolately_package_source = 'http://ip-172-31-4-194.ap-southeast-2.compute.internal/choco/',
+  String $chocolately_package_source = 'c:\\choco_packages\\',
   String $chocolatey_download_url = 'http://ip-172-31-4-194.ap-southeast-2.compute.internal/chocolatey.0.11.2.nupkg',
+  String $choco_http_source = 'http://ip-172-31-4-194.ap-southeast-2.compute.internal/choco/',
+  Array $packges_to_sync = ['Wget.1.21.2.nupkg','adoptopenjdk8jre.8.292.10.nupkg'],
 ) {
 
   class { 'chocolatey':
@@ -18,5 +20,13 @@ class profile::base_chocolatey (
 
   chocolateysource {'chocolatey':
     ensure   => disabled,
+  }
+
+  $packges_to_sync.each | String $package | {
+    file { $package:
+      ensure => file,
+      source => "${choco_http_source}${package}",
+      path   => $chocolately_package_source,
+    }
   }
 }
