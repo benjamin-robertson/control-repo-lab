7155eabce8a19f49464e5e0825677c4a19aacc6f
Benjamin Robertson <ben.robertson@puppet.com>
2021-11-02 15:01:15 +1100

    enabeld iis

--- site-modules/profile/manifests/base_windows.pp
+++ site-modules/profile/manifests/base_windows.pp
@@ -17,8 +17,8 @@ class profile::base_windows {
   include profile::windows::logon_as_service
   include profile::windows::tempdir_permissions
   include profile::windows::registry
-  #include profile::windows::iis
-  #include profile::windows::7zip
+  include profile::windows::iis
+  include profile::windows::7zip
   include profile::windows::choco
   include profile::windows::scope_test
   include profile::windows::hosts
