9130848bad4d173e07bf8b138732920047483388
Benjamin Robertson <ben.robertson@puppet.com>
2021-11-08 16:11:31 +1100

    fix

--- site-modules/profile/manifests/windows_applocker_fervid.pp
+++ site-modules/profile/manifests/windows_applocker_fervid.pp
@@ -4,10 +4,10 @@
 class profile::windows_applocker_fervid {
   #include puppet_applocker_fervid
   class { 'acsc_e8_application_control':
-    executable_rules   => 'Enforce',
-    msi_rules          => 'Enforce',
-    dll_rules          => 'Enforce',
-    script_rules       => 'Enforce',
-    packaged_app_rules => 'Enforce',
+    executable_rules   => 'Enabled',
+    msi_rules          => 'Enabled',
+    dll_rules          => 'Enabled',
+    script_rules       => 'Enabled',
+    packaged_app_rules => 'Enabled',
   }
 }
