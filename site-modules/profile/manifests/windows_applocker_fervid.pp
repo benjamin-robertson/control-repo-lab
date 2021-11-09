c6bfd5d8fccbc5218c72067e071e076cc8db4919
Benjamin Robertson <ben.robertson@puppet.com>
2021-11-09 23:56:42 +1100

    Switch from audit mode

--- site-modules/profile/manifests/windows_applocker_fervid.pp
+++ site-modules/profile/manifests/windows_applocker_fervid.pp
@@ -3,35 +3,35 @@
 #
 class profile::windows_applocker_fervid {
   #include acsc_e8_application_control
-class { 'acsc_e8_application_control':
-  additional_exec_applocker_rules => {
-    'Exec c:\\temp' => {
-        'ensure'            => 'absent',
-        'action'            => 'Allow',
-      'conditions' => {
-        'path'              => '%OSDRIVE%\\temp\\*',
-      },
-        'description'       => 'Allow all users to run executable from c:\\temp',
-        'rule_type'         => 'path',
-        'type'              => 'Exe',
-        'user_or_group_sid' => 'S-1-1-0',
-      },
-    },
-    executable_rules   => 'Audit',
-    msi_rules          => 'Audit',
-    dll_rules          => 'Audit',
-    script_rules       => 'Audit',
-    packaged_app_rules => 'Audit',
-    start_service      => true,
-  }
-
 #class { 'acsc_e8_application_control':
-#    executable_rules   => 'Enabled',
-#    msi_rules          => 'Enabled',
-#    dll_rules          => 'Enabled',
-#    script_rules       => 'Enabled',
-#    packaged_app_rules => 'Enabled',
+#  additional_exec_applocker_rules => {
+#    'Exec c:\\temp' => {
+#        'ensure'            => 'absent',
+#        'action'            => 'Allow',
+#      'conditions' => {
+#        'path'              => '%OSDRIVE%\\temp\\*',
+#      },
+#        'description'       => 'Allow all users to run executable from c:\\temp',
+#        'rule_type'         => 'path',
+#        'type'              => 'Exe',
+#        'user_or_group_sid' => 'S-1-1-0',
+#      },
+#    },
+#    executable_rules   => 'Audit',
+#    msi_rules          => 'Audit',
+#    dll_rules          => 'Audit',
+#    script_rules       => 'Audit',
+#    packaged_app_rules => 'Audit',
 #    start_service      => true,
 #  }
+
+  class { 'acsc_e8_application_control':
+    executable_rules   => 'Enabled',
+    msi_rules          => 'Enabled',
+    dll_rules          => 'Enabled',
+    script_rules       => 'Enabled',
+    packaged_app_rules => 'Enabled',
+    start_service      => true,
+  }
 
 }
