e0c4832f2cb94ccd2705c7b362afde2612302b12
Benjamin Robertson <ben.robertson@puppet.com>
2021-11-09 11:27:00 +1100

    Add extra applocker rule

--- site-modules/profile/manifests/windows_applocker_fervid.pp
+++ site-modules/profile/manifests/windows_applocker_fervid.pp
@@ -3,11 +3,25 @@
 #
 class profile::windows_applocker_fervid {
   #include puppet_applocker_fervid
-  class { 'acsc_e8_application_control':
+class { 'acsc_e8_application_control':
+  additional_exec_applocker_rules => {
+    'Exec c:\temp' => {
+        'ensure'            => 'present',
+        'action'            => 'Allow',
+      'conditions' => {
+        'path'              => 'c:\temp',
+      },
+        'description'       => 'Allow all users to run executable from c:\temp',
+        'rule_type'         => 'path',
+        'type'              => 'Exe',
+        'user_or_group_sid' => 'S-1-1-0',
+      },
+      },
     executable_rules   => 'Enabled',
     msi_rules          => 'Enabled',
     dll_rules          => 'Enabled',
     script_rules       => 'Enabled',
     packaged_app_rules => 'Enabled',
+    start_service      => true,
   }
 }
