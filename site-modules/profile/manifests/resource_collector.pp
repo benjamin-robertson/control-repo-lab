8a532387dff2dfff8af978943e644ad411d11113
Benjamin Robertson <ben.robertson@puppet.com>
2021-12-09 12:12:05 +1100

    Collect resources

--- site-modules/profile/manifests/resource_collector.pp
+++ site-modules/profile/manifests/resource_collector.pp
@@ -4,4 +4,12 @@
 #
 class profile::resource_collector {
   notify { 'Collect all the resources':}
+
+  @@host { $facts['fqdn']:
+    ip           => $facts['ipaddress'],
+    host_aliases => $facts['hostname'],
+  }
+
+  # collect all exported resources and realize them on this host
+  Host <<||>>
 }
