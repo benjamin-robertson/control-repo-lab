f7f0304501afcb9af0106bf6e24bf6e4c516bb2e
Benjamin Robertson <ben.robertson@puppet.com>
2021-12-13 14:46:52 +1100

    Assigned default

--- site-modules/profile/manifests/office_hardening.pp
+++ site-modules/profile/manifests/office_hardening.pp
@@ -2,7 +2,7 @@
 #
 #
 class profile::office_hardening (
-  Enum['all_macros_disabled','macros_from_trused_locations','signed_macros_only','clear_macro_settings'] $macro_setting
+  Enum['all_macros_disabled','macros_from_trused_locations','signed_macros_only','clear_macro_settings'] $macro_setting = 'clear_macro_settings'
 ){
   class {'acsc_e8_office_hardening':
     macro_setting => $macro_setting,
