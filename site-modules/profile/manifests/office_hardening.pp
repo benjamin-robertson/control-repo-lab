bf371b6496e3f07a4489881078b416f00f2097c1
Benjamin Robertson <ben.robertson@puppet.com>
2021-12-13 16:16:51 +1100

    Added trusted location

--- site-modules/profile/manifests/office_hardening.pp
+++ site-modules/profile/manifests/office_hardening.pp
@@ -2,9 +2,11 @@
 #
 #
 class profile::office_hardening (
-  Enum['all_macros_disabled','macros_from_trused_locations','signed_macros_only','clear_macro_settings'] $macro_setting = 'clear_macro_settings'
+  Enum['all_macros_disabled','macros_from_trused_locations','signed_macros_only','clear_macro_settings'] $macro_setting = 'clear_macro_settings',
+  Hash $trusted_locations = {}
 ){
   class {'acsc_e8_office_hardening':
-    macro_setting => $macro_setting,
+    macro_setting     => $macro_setting,
+    trusted_locations => $trusted_locations,
   }
 }
