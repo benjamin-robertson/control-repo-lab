468385c239eaf39a1da869eff39fe659b3e30457
Benjamin Robertson <ben.robertson@puppet.com>
2021-12-24 17:30:53 +1100

    Update paramater

--- site-modules/profile/manifests/office_hardening.pp
+++ site-modules/profile/manifests/office_hardening.pp
@@ -2,7 +2,7 @@
 #
 #
 class profile::office_hardening (
-  Enum['all_macros_disabled','macros_from_trused_locations','signed_macros_only','clear_macro_settings'] $macro_setting = 'all_macros_disabled',
+  Enum['all_macros_disabled','macros_from_trusted_locations','signed_macros_only','clear_macro_settings'] $macro_setting = 'all_macros_disabled',
   Hash $trusted_locations = {}
 ){
   class {'acsc_e8_office_hardening':
