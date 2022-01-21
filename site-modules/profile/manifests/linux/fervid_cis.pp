4c28280eda997fc72013ee8ae0e666523c317405
Benjamin Robertson <ben.robertson@puppet.com>
2022-01-21 13:59:35 +1100

    updated array

--- site-modules/profile/manifests/linux/fervid_cis.pp
+++ site-modules/profile/manifests/linux/fervid_cis.pp
@@ -7,6 +7,6 @@ class profile::linux::fervid_cis {
   class {'::secure_linux_cis':
     time_servers => ['169.254.169.123', 'ntp.ubuntu.com'],
     profile_type => 'server',
-    allow_users  => 'ubuntu',
+    allow_users  => ['ubuntu'],
   }
 }
