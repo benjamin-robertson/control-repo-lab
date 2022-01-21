9b7470926e0f6c6c233862f41d952fba2fdcdd6d
Benjamin Robertson <ben.robertson@puppet.com>
2022-01-21 13:52:12 +1100

    Added fervid cis

---
+++ site-modules/profile/manifests/linux/fervid_cis.pp
@@ -0,0 +1,12 @@
+# Class: profile::linux::fervid_cis
+#
+# fervid on ubunut 20.04
+#
+class profile::linux::fervid_cis {
+
+  class {'::secure_linux_cis':
+    time_servers => ['169.254.169.123', 'ntp.ubuntu.com'],
+    profile_type => 'server',
+    allow_users  => 'ubuntu',
+  }
+}
