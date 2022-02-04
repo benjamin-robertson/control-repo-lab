2bf8639da1cf4f72a448b716ee525398334accfa
Benjamin Robertson <ben.robertson@puppet.com>
2022-02-04 13:33:12 +1100

    join domain

--- site-modules/profile/manifests/windows_domain_join.pp
+++ site-modules/profile/manifests/windows_domain_join.pp
@@ -11,12 +11,20 @@ class profile::windows_domain_join (
   # Setup DNS servers
   $interfaces = $facts['networking']['interfaces']
   $interfaces.each | String $name, Hash $hash | {
-    notify { "My interface names are ${name}": }
     dsc_dnsserveraddress { 'AD DNS':
       dsc_address        => $dns_server_addresses,
       dsc_addressfamily  => 'IPv4',
       dsc_interfacealias => $name,
+      before             => Dsc_computer['domain join'],
     }
   }
 
+  # Join the domain
+  dsc_computer { 'domain join':
+    dsc_domainname  => $domain_name,
+    dsc_description => 'Joined to domain by Puppet',
+    dsc_credential  => { 'user' => $domain_bind_user, 'password' => $domain_bind_password },
+    dsc_name        => $facts['clientcert'],
+    validation_mode => 'resource',
+  }
 }
