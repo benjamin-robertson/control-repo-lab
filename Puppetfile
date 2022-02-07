69b94f46a620d09285b65836f5b943806b8b6d45
Benjamin Robertson <ben.robertson@puppet.com>
2022-02-07 11:18:17 +1100

    Add rdp firewall rule

--- Puppetfile
+++ Puppetfile
@@ -46,7 +46,7 @@ mod 'puppetlabs-sshkeys_core', '2.2.0'
 mod 'puppetlabs-stdlib', '7.1.0'
 mod 'WhatsARanjit-node_manager', '0.7.3'
 #mod 'webalex-windows_firewall', '0.2.0'
-mod 'puppet-windows_firewall', '3.0.0'
+mod 'puppet-windows_firewall', '4.0.0'
 mod 'puppetlabs-sqlserver', '3.0.0'
 mod 'puppetlabs-wsus_client', '4.0.0'
 
--- site-modules/profile/manifests/base_hardened_windows.pp
+++ site-modules/profile/manifests/base_hardened_windows.pp
@@ -3,4 +3,17 @@
 #
 class profile::base_hardened_windows {
   include cem_windows
+
+  # Permit RDP inbound
+  windows_firewall::exception { 'Remote desktop':
+    ensure       => present,
+    direction    => 'in',
+    action       => 'allow',
+    enabled      => true,
+    protocol     => 'TCP',
+    local_port   => 3389,
+    remote_port  => 'any',
+    display_name => 'Remote desktop',
+    description  => 'Permit remote desktop',
+  }
 }
