d69e98a47dc550938072cf609132348d39917125
Benjamin Robertson <ben.robertson@puppet.com>
2022-02-28 12:47:02 +1100

    test

--- site-modules/profile/manifests/linux/motd_linux.pp
+++ site-modules/profile/manifests/linux/motd_linux.pp
@@ -4,6 +4,9 @@ class profile::linux::motd_linux (
   String $motdcontent = 'test Paramater'
 ) {
   #$motdcontent = lookup(motd_linux)
+  $uptimeseonds = $facts['uptime_seconds'] / 3600
+  notify {"${uptime_seconds}":}
+  #if $facts[]
   Class { 'motd':
     content => $motdcontent,
   }
