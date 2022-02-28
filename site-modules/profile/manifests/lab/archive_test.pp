8b545c52664e61f8a0b7ceb8cfe0e3633c00513c
Benjamin Robertson <ben.robertson@puppet.com>
2022-02-28 15:31:42 +1100

    test

--- site-modules/profile/manifests/lab/archive_test.pp
+++ site-modules/profile/manifests/lab/archive_test.pp
@@ -4,7 +4,8 @@
 # 
 class profile::lab::archive_test {
   archive { 'test file':
-    path   => '/tmp/myfile.txt',
-    source => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt'
+    path         => '/tmp/myfile.txt',
+    source       => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
+    checksum_url => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
   }
 }
