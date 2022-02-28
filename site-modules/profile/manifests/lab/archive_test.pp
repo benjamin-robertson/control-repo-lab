03330a839c3d44f48b4ffdb8ffe1657f1f7f43e1
Benjamin Robertson <ben.robertson@puppet.com>
2022-02-28 16:10:15 +1100

    uplift

--- site-modules/profile/manifests/lab/archive_test.pp
+++ site-modules/profile/manifests/lab/archive_test.pp
@@ -6,8 +6,8 @@ class profile::lab::archive_test {
   archive { 'test file':
     path          => '/tmp/myfile.txt',
     source        => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
-    checksum_url  => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
-    checksum_type => 'sha1',
+    #checksum_url  => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
+    #checksum_type => 'sha1',
   }
   #file { 'test file':
   #  path   => '/tmp/myfile.txt',
