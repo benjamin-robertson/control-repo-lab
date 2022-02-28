3fc723043bd8478a87ad8993ba2aaee0c33b295d
Benjamin Robertson <ben.robertson@puppet.com>
2022-02-28 16:10:04 +1100

    uplift archive

--- site-modules/profile/manifests/lab/archive_test.pp
+++ site-modules/profile/manifests/lab/archive_test.pp
@@ -3,16 +3,16 @@
 # testing the archive module
 # 
 class profile::lab::archive_test {
-  #archive { 'test file':
-  #  path          => '/tmp/myfile.txt',
-  #  source        => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
-  #  checksum_url  => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
-  #  checksum_type => 'sha1',
-  #}
-  file { 'test file':
-    path   => '/tmp/myfile.txt',
-    source => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
+  archive { 'test file':
+    path          => '/tmp/myfile.txt',
+    source        => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
+    checksum_url  => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
+    checksum_type => 'sha1',
   }
+  #file { 'test file':
+  #  path   => '/tmp/myfile.txt',
+  #  source => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
+  #}
   file { 'deb file':
     path   => '/tmp/amazon-ssm-agent.deb',
     source => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/amazon-ssm-agent.deb',
