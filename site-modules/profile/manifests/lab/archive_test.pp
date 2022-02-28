e59d3a05f49c7abd942c3c060ffd4b81e8d4baa1
Benjamin Robertson <ben.robertson@puppet.com>
2022-02-28 15:22:04 +1100

    create archive test

---
+++ site-modules/profile/manifests/lab/archive_test.pp
@@ -0,0 +1,10 @@
+# Class: profile::lab::archive_test
+#
+# testing the archive module
+# 
+class profile::lab::archive_test {
+  archive { 'test file':
+    path   => '/tmp/myfile.txt',
+    source => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt'
+  }
+}
