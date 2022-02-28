308c5817898bace2e53e0658aa34cc3cbaba4f2b
Benjamin Robertson <ben.robertson@puppet.com>
2022-02-28 15:36:11 +1100

    checksum type

--- site-modules/profile/manifests/lab/archive_test.pp
+++ site-modules/profile/manifests/lab/archive_test.pp
@@ -4,8 +4,9 @@
 # 
 class profile::lab::archive_test {
   archive { 'test file':
-    path         => '/tmp/myfile.txt',
-    source       => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
-    checksum_url => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
+    path          => '/tmp/myfile.txt',
+    source        => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
+    checksum_url  => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
+    checksum_type => 'sha1',
   }
 }
