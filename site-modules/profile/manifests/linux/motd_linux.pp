f993986a2d6473c7faa3aa6f7da4553f7de10750
Benjamin Robertson <ben.robertson@puppet.com>
2022-02-28 12:18:47 +1100

    update motd

--- site-modules/profile/manifests/linux/motd_linux.pp
+++ site-modules/profile/manifests/linux/motd_linux.pp
@@ -1,9 +1,9 @@
 # Class: profile::linux::m
 #
 class profile::linux::motd_linux (
-  #String $motdcontent = 'test Paramater'
+  String $motdcontent = 'test Paramater'
 ) {
-  $motdcontent = lookup(motd_linux)
+  #$motdcontent = lookup(motd_linux)
   Class { 'motd':
     content => $motdcontent,
   }
