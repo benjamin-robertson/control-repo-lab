f7095755b7a967c3a573f41dde4c330a82e37012
Benjamin Robertson <ben.robertson@puppet.com>
2022-03-01 13:09:17 +1100

    fix

--- site-modules/profile/manifests/lab/archive_test.pp
+++ site-modules/profile/manifests/lab/archive_test.pp
@@ -28,10 +28,10 @@ class profile::lab::archive_test {
 
   file { 'test file':
     path   => '/tmp/myfile.txt',
-    source => 'http://webserver.com/myfile.txt',
+    source => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
   }
   file { 'deb file':
     path   => '/tmp/amazon-ssm-agent.deb',
-    source => 'http://webserver.com/amazon-ssm-agent.deb',
+    source => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/amazon-ssm-agent.deb',
   }
 }
