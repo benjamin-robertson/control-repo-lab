8086f0669eb68f630cd5dd8b81bd5bb8a83ef60f
Benjamin Robertson <ben.robertson@puppet.com>
2022-03-01 11:34:56 +1100

    push changes

--- site-modules/profile/manifests/lab/archive_test.pp
+++ site-modules/profile/manifests/lab/archive_test.pp
@@ -3,18 +3,18 @@
 # testing the archive module
 # 
 class profile::lab::archive_test {
-  archive { 'test file':
-    path          => '/tmp/myfile.txt',
-    source        => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
+  #archive { 'test file':
+  #  path          => '/tmp/myfile.txt',
+  #  source        => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
     #checksum      => 'ebccea7a808019a997af5e6eeeb4b710',
     #checksum_type => 'md5',
     #checksum_url  => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
     #checksum_type => 'sha1',
-  }
-  #file { 'test file':
-  #  path   => '/tmp/myfile.txt',
-  #  source => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
   #}
+  file { 'test file':
+    path   => '/tmp/myfile.txt',
+    source => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/myfile.txt',
+  }
   file { 'deb file':
     path   => '/tmp/amazon-ssm-agent.deb',
     source => 'http://ip-172-31-11-63.ap-southeast-2.compute.internal/amazon-ssm-agent.deb',
