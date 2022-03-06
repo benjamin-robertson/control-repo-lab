2c763f9a9584f92d925d4be24416c6d989971a1a
Benjamin Robertson <ben.robertson@puppet.com>
2022-03-07 10:29:34 +1100

    added ca content

--- site-modules/profile/manifests/lab/gitlab_runner.pp
+++ site-modules/profile/manifests/lab/gitlab_runner.pp
@@ -2,6 +2,12 @@
 #
 # Install gitlab runner
 #
-class profile::lab::gitlab_runner {
+class profile::lab::gitlab_runner (
+  String $ca_content,
+) {
   include gitlab_ci_runner
+
+  file {'/tmp/mycertificate.crt':
+    content => $ca_content,
+  }
 }
