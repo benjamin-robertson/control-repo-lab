5b27e806a171f5c0911d3fcb366a0e5a4ef02a34
Benjamin Robertson <ben.robertson@puppet.com>
2022-03-07 14:35:45 +1100

    specifiy curl

--- site-modules/profile/manifests/lab/gitlab_runner.pp
+++ site-modules/profile/manifests/lab/gitlab_runner.pp
@@ -6,6 +6,7 @@ class profile::lab::gitlab_runner (
   String $ca_content,
   String $pe_client_tools_download_location = 'https://pm.puppetlabs.com/pe-client-tools/2019.8.10/19.8.11/repos/deb/focal/PC1/pe-client-tools_19.8.11-1focal_amd64.deb',
   Array $pe_servers = ['ip-172-31-28-67.ap-southeast-2.compute.internal'],
+  String $save_location = '/home/gitlab-runner',
 ) {
   include gitlab_ci_runner
 
@@ -13,20 +14,20 @@ class profile::lab::gitlab_runner (
     content => $ca_content,
   }
 
-  file { '/tmp/pe-client-tools_19.8.11-1focal_amd64.deb':
+  file { "${$save_location}/pe-client-tools_19.8.11-1focal_amd64.deb":
     source => $pe_client_tools_download_location,
   }
 
   package {'pe-client-tools':
     ensure   => 'latest',
-    source   => '/tmp/pe-client-tools_19.8.11-1focal_amd64.deb',
+    source   => "${$save_location}/pe-client-tools_19.8.11-1focal_amd64.deb",
     provider => 'dpkg',
   }
 
   $pe_servers.each | Integer $count, String $server | {
     exec {"Download pe certificate ${count}":
-      command => "curl https://${$server}:8140/puppet-ca/v1/certificate/ca -k > /home/gitlab-runner/${$server}.pem",
-      creates => "/home/gitlab-runner/${$server}.pem",
+      command => "/usr/bin/curl https://${$server}:8140/puppet-ca/v1/certificate/ca -k > ${$save_location}/${$server}.pem",
+      creates => "${$save_location}/${$server}.pem",
     }
   }
 
