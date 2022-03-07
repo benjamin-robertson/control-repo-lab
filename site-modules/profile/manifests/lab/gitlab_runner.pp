d5699e6f73ca432ce7245352e954d9a0074c077d
Benjamin Robertson <ben.robertson@puppet.com>
2022-03-07 13:55:30 +1100

    test download

--- site-modules/profile/manifests/lab/gitlab_runner.pp
+++ site-modules/profile/manifests/lab/gitlab_runner.pp
@@ -4,10 +4,15 @@
 #
 class profile::lab::gitlab_runner (
   String $ca_content,
+  String $pe_client_tools_download_location = 'https://pm.puppetlabs.com/pe-client-tools/2019.8.10/19.8.11/repos/deb/focal/PC1/pe-client-tools_19.8.11-1focal_amd64.deb',
 ) {
   include gitlab_ci_runner
 
   file {'/etc/ssl/certs/mycoolca.pem':
     content => $ca_content,
+  }
+
+  file {'/tmp/pe-client-tools_19.8.11-1focal_amd64.deb':
+    source => $pe_client_tools_download_location,
   }
 }
