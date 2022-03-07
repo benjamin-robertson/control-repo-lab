0a58febafd15dde513d90e06a95efed3a22f28c1
Benjamin Robertson <ben.robertson@puppet.com>
2022-03-07 14:30:28 +1100

    added creates

--- site-modules/profile/manifests/lab/gitlab_runner.pp
+++ site-modules/profile/manifests/lab/gitlab_runner.pp
@@ -5,6 +5,7 @@
 class profile::lab::gitlab_runner (
   String $ca_content,
   String $pe_client_tools_download_location = 'https://pm.puppetlabs.com/pe-client-tools/2019.8.10/19.8.11/repos/deb/focal/PC1/pe-client-tools_19.8.11-1focal_amd64.deb',
+  Array $pe_servers = ['ip-172-31-28-67.ap-southeast-2.compute.internal'],
 ) {
   include gitlab_ci_runner
 
@@ -21,4 +22,12 @@ class profile::lab::gitlab_runner (
     source   => '/tmp/pe-client-tools_19.8.11-1focal_amd64.deb',
     provider => 'dpkg',
   }
+
+  $pe_servers.each | Integer $count, String $server | {
+    exec {"Download pe certificate ${count}":
+      command => "curl https://${$server}:8140/puppet-ca/v1/certificate/ca -k > /home/gitlab-runner/${$server}.pem",
+      creates => "/home/gitlab-runner/${$server}.pem",
+    }
+  }
+
 }
