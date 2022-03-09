818bbdbe788c18a09de810a08471fff1b9b8b2a9
Benjamin Robertson <ben.robertson@puppet.com>
2022-03-09 12:48:47 +1100

    with notify

--- site-modules/profile/manifests/lab/proxy_setup.pp
+++ site-modules/profile/manifests/lab/proxy_setup.pp
@@ -31,12 +31,12 @@ class profile::lab::proxy_setup (
   file { '/etc/apt/apt.conf.d/01proxy1':
     ensure  => file,
     content => 'Acquire::http::proxy "http://ip-172-31-11-63.ap-southeast-2.compute.internal:3128/";',
-    notify  => Exec['apt update 1st stage'],
+    #notify  => Exec['apt update 1st stage'],
   }
 
   exec { 'apt update 1st stage':
     command     => '/usr/bin/apt update',
-    refreshonly => true,
+    #refreshonly => true,
   }
 
   #file { 'test file':
