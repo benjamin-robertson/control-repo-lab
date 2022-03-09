07b619bef30ed696b4ebf3fd67d7f07682253938
Benjamin Robertson <ben.robertson@puppet.com>
2022-03-09 13:49:18 +1100

    switch back to file

--- site-modules/profile/manifests/lab/proxy_setup.pp
+++ site-modules/profile/manifests/lab/proxy_setup.pp
@@ -28,11 +28,17 @@ class profile::lab::proxy_setup (
     content => $proxy_file,
   }
 
-  exec { 'echo /etc/apt/apt.conf.d/01proxy1':
-    command => '/usr/bin/echo "Acquire::http::proxy \"http://ip-172-31-11-63.ap-southeast-2.compute.internal:3128/\";" > /etc/apt/apt.conf.d/01proxy1',
-    creates => '/etc/apt/apt.conf.d/01proxy1',
+  file {'stage 1 /etc/apt/apt.conf.d/01proxy1':
+    path    => '/etc/apt/apt.conf.d/01proxy1',
+    content => 'Acquire::http::proxy "http://ip-172-31-11-63.ap-southeast-2.compute.internal:3128/";',
     notify  => Exec['apt update 1st stage'],
   }
+
+  #exec { 'echo /etc/apt/apt.conf.d/01proxy1':
+  #  command => '/usr/bin/echo "Acquire::http::proxy \"http://ip-172-31-11-63.ap-southeast-2.compute.internal:3128/\";" > /etc/apt/apt.conf.d/01proxy1',
+  #  creates => '/etc/apt/apt.conf.d/01proxy1',
+  #  notify  => Exec['apt update 1st stage'],
+  #}
 
   exec { 'apt update 1st stage':
     command     => '/usr/bin/apt update',
