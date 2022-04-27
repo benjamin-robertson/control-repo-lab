9b0a70dfef3640ba391c7b57c55a28478424d948
Benjamin Robertson <ben.robertson@puppet.com>
2022-04-27 10:50:20 +1000

    added host entry

---
+++ site-modules/profile/manifests/linux/hosts.pp
@@ -0,0 +1,11 @@
+# Class: profile::linux::hosts
+#
+#
+class profile::linux::hosts {
+  # Collect all the hosts
+  host { 'hostname':
+    name    => $facts['hostname'],
+    ip      => $facts['ipaddress'],
+    comment => 'exported resource'
+  }
+}
