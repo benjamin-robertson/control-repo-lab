06d7ad119cd799b2be3a5412b3770ee4e735d7bc
Benjamin Robertson <ben.robertson@puppet.com>
2022-04-27 17:14:27 +1000

    chnage to public hostname

--- site-modules/profile/manifests/linux/gitlab.pp
+++ site-modules/profile/manifests/linux/gitlab.pp
@@ -4,7 +4,7 @@
 class profile::linux::gitlab {
   # install gitlab ee
   class { 'gitlab':
-    external_url            => "https://${facts['fqdn']}",
+    external_url            => "https://${facts['ec2-metadata']['public-hostname']}",
     manage_upstream_edition => 'ee',
   }
 }
