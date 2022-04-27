c8bf14587fbe9aeb42e89190627f37e52bdf2ddf
Benjamin Robertson <ben.robertson@puppet.com>
2022-04-27 16:12:28 +1000

    Setup gitlab testing

---
+++ site-modules/profile/manifests/linux/gitlab.pp
@@ -0,0 +1,10 @@
+# Class: profile::linux::gitlab
+#
+#
+class profile::linux::gitlab {
+  # install gitlab ee
+  class { 'gitlab':
+    external_url            => 'ec2-3-25-233-142.ap-southeast-2.compute.amazonaws.com',
+    manage_upstream_edition => 'ee',
+  }
+}
