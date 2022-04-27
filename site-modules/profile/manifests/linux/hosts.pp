506d78b278fa36c7b3f4273819774d667c517775
Benjamin Robertson <ben.robertson@puppet.com>
2022-04-27 10:53:18 +1000

    Exported resources

--- site-modules/profile/manifests/linux/hosts.pp
+++ site-modules/profile/manifests/linux/hosts.pp
@@ -3,9 +3,10 @@
 #
 class profile::linux::hosts {
   # Collect all the hosts
-  host { 'hostname':
+  @@host { 'hostname':
     name    => $facts['hostname'],
     ip      => $facts['ipaddress'],
     comment => 'exported resource'
   }
+  Host <<| |>>
 }
