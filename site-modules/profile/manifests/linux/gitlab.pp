21fc533134a74a4d20285694380b4b1ca6fbd3e7
Benjamin Robertson <ben.robertson@puppet.com>
2022-04-27 17:22:31 +1000

    Fixed underscore

--- site-modules/profile/manifests/linux/gitlab.pp
+++ site-modules/profile/manifests/linux/gitlab.pp
@@ -4,10 +4,10 @@
 class profile::linux::gitlab {
   # install gitlab ee
 
-  notify { "This is ec2 ${facts['ec2-metadata']}":}
+  #notify { "This is ec2 ${facts['ec2-metadata']}":}
 
-  # class { 'gitlab':
-  #   external_url            => "https://${facts['ec2-metadata']['public-hostname']}",
-  #   manage_upstream_edition => 'ee',
-  # }
+  class { 'gitlab':
+    external_url            => "https://${facts['ec2_metadata']['public-hostname']}",
+    manage_upstream_edition => 'ee',
+  }
 }
