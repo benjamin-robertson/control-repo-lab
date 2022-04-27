5e946ce9780a7553e76f38035debfa73f33289ec
Benjamin Robertson <ben.robertson@puppet.com>
2022-04-27 13:09:30 +1000

    Switch to latest

--- site-modules/profile/manifests/linux/vcs.pp
+++ site-modules/profile/manifests/linux/vcs.pp
@@ -3,7 +3,7 @@
 #
 class profile::linux::vcs {
   vcsrepo { '/tmp/dogsite':
-    ensure   => present,
+    ensure   => latest,
     provider => git,
     source   => 'https://github.com/benjamin-robertson/dog-site.git',
     branch   => 'main',
