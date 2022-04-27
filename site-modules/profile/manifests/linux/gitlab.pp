04e7c987351ebfd1515d4221f1c4a60efd022bba
Benjamin Robertson <ben.robertson@puppet.com>
2022-04-27 16:18:57 +1000

    Update external url

--- site-modules/profile/manifests/linux/gitlab.pp
+++ site-modules/profile/manifests/linux/gitlab.pp
@@ -4,7 +4,7 @@
 class profile::linux::gitlab {
   # install gitlab ee
   class { 'gitlab':
-    external_url            => 'ec2-3-25-233-142.ap-southeast-2.compute.amazonaws.com',
+    external_url            => 'https://ec2-3-25-233-142.ap-southeast-2.compute.amazonaws.com',
     manage_upstream_edition => 'ee',
   }
 }
