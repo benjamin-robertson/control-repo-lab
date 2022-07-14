5723601bd9677392bbe225d84a46576e985818e1
Benjamin Robertson <ben.robertson@puppet.com>
2022-07-14 16:09:35 +1000

    purge none

--- site-modules/profile/manifests/pe_master/node_groups.pp
+++ site-modules/profile/manifests/pe_master/node_groups.pp
@@ -8,6 +8,7 @@ class profile::pe_master::node_groups {
     override_environment => 'false',
     parent               => 'All Nodes',
     provider             => 'https',
+    purge_behavior       => 'none',
     rule                 => ['or',
     ['=', 'name', 'ip-172-31-28-67.ap-southeast-2.compute.internal']],
   }
