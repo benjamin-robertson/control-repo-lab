6ff346e941f7f9dce8a7fbe14c5428e0a7c4e22d
Benjamin Robertson <ben.robertson@puppet.com>
2022-07-14 16:00:27 +1000

    Removed id

--- site-modules/profile/manifests/pe_master/node_groups.pp
+++ site-modules/profile/manifests/pe_master/node_groups.pp
@@ -5,7 +5,6 @@ class profile::pe_master::node_groups {
   node_group { 'test_group':
     ensure               => 'present',
     environment          => 'production',
-    id                   => '0f76f131-b064-4a83-bdb3-ddb1d4b7c530',
     override_environment => 'false',
     parent               => 'All Nodes',
     provider             => 'https',
