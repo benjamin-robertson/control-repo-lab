d52e4889a281ecac02fda3c1aea2c6388f6a7fe3
Benjamin Robertson <ben.robertson@puppet.com>
2022-07-20 10:45:21 +1000

    Rename louie

--- site-modules/profile/manifests/pe_master/node_groups.pp
+++ site-modules/profile/manifests/pe_master/node_groups.pp
@@ -13,7 +13,7 @@ class profile::pe_master::node_groups {
   ['and',
     ['=',
       ['fact', 'aio_agent_version'],
-      'louie1']],
+      'louie']],
   ['=', 'name', 'ip-172-31-28-67.ap-southeast-2.compute.internal']],
   }
 
