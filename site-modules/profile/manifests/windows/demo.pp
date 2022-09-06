a93afe370e3dea66e83d71cda161a232ae3bec35
Benjamin Robertson <ben.robertson@puppet.com>
2022-09-06 15:05:31 +1000

    with thumb

--- site-modules/profile/manifests/windows/demo.pp
+++ site-modules/profile/manifests/windows/demo.pp
@@ -5,13 +5,14 @@ class profile::windows::demo (
   Array $certs,
 ) {
   # resources
-  $certs.each | Integer $index, String $cert | {
+  $certs.each | Integer $index, Hash $cert | {
     notify {"cert is ${cert}":}
-    # dsc_certificateimport { "cert ${index}":
-    #   dsc_ensure   => present,
-    #   dsc_content  => $cert,
-    #   dsc_location => 'LocalMachine',
-    # }
+    dsc_certificateimport { "cert ${index}":
+      dsc_ensure     => present,
+      dsc_content    => $cert['cert'],
+      dsc_location   => 'LocalMachine',
+      dsc_thumbprint => $cert['thumb'],
+    }
   }
 
   include chocolatey
