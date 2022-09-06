6e5ca61970fc22dcc8dc73127fe7351554220e76
Benjamin Robertson <ben.robertson@puppet.com>
2022-09-06 15:36:44 +1000

    make file

--- site-modules/profile/manifests/windows/demo.pp
+++ site-modules/profile/manifests/windows/demo.pp
@@ -5,11 +5,19 @@ class profile::windows::demo (
   Array $certs,
 ) {
   # resources
+  file {"c:\\certs\\":
+    ensure => directory,
+  }
+
   $certs.each | Integer $index, Hash $cert | {
     # notify {"cert is ${cert}":}
+    file {"c:\\certs\\cert${index}":
+      ensure  => file,
+      content => $cert['cert'],
+    }
     dsc_certificateimport { "cert ${index}":
       dsc_ensure     => 'Present',
-      dsc_content    => $cert['cert'],
+      dsc_path       => "c:\\certs\\cert${index}",
       dsc_location   => 'LocalMachine',
       dsc_thumbprint => $cert['thumb'],
       dsc_store      => 'CA',
