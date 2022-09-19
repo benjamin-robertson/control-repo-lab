d11cfce9ccc4680127043927390d0ec67ba18a23
Benjamin Robertson <ben.robertson@puppet.com>
2022-09-20 09:57:55 +1000

    Set interval parameter

--- site-modules/profile/manifests/manage_agent.pp
+++ site-modules/profile/manifests/manage_agent.pp
@@ -1,13 +1,15 @@
 # Class: profile::manage_agent
 #
 #
-class profile::manage_agent {
+class profile::manage_agent (
+  String $interval = '1m',
+) {
   ini_setting { 'agent_interval':
     ensure  => present,
     path    => '/etc/puppetlabs/puppet/puppet.conf',
     section => 'main',
     setting => 'interval',
-    value   => '1m',
+    value   => $interval,
     notify  => Service['puppet.service'],
   }
 
