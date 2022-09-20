06c1f72ecfa8abba5ed337349338c5d4ea21e2d3
Benjamin Robertson <ben.robertson@puppet.com>
2022-09-20 16:11:18 +1000

    no array

--- site-modules/profile/manifests/windows/audit_dsc.pp
+++ site-modules/profile/manifests/windows/audit_dsc.pp
@@ -5,7 +5,7 @@ class profile::windows::audit_dsc {
   # resources
   dsc_auditpolicysubcategory { 'Security System Extension':
     dsc_ensure      => present,
-    dsc_auditflag   => ['Failure', 'Success'],
+    dsc_auditflag   => 'Failure',
     dsc_name        => 'Security System Extension',
     validation_mode => 'resource',
   }
