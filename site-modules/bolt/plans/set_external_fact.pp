d0486b3be0776ac52c4a26d4e57eb649f82908d7
Benjamin Robertson <ben.robertson@puppet.com>
2022-09-28 16:41:01 +1000

    remove apply

--- site-modules/bolt/plans/set_external_fact.pp
+++ site-modules/bolt/plans/set_external_fact.pp
@@ -4,41 +4,41 @@ plan bolt::set_external_fact (
   String     $fact_name   = 'code_release',
   Boolean    $noop        = false,
 ) {
-  # $full_list = get_targets($targets)
-  # unless $full_list.empty {
+  $full_list = get_targets($targets)
+  unless $full_list.empty {
 
-  #   # supported platforms
-  #   $supported_platforms = ['Debian', 'RedHat']
+    # supported platforms
+    $supported_platforms = ['Debian', 'RedHat']
 
-  #   $supported_targets = get_targets($full_list).filter | $target | {
-  #     $target.facts['os']['family'] in $supported_platforms
-  #   }
+    $supported_targets = get_targets($full_list).filter | $target | {
+      $target.facts['os']['family'] in $supported_platforms
+    }
 
-  #   out::message("Supported targets are ${supported_targets}")
+    out::message("Supported targets are ${supported_targets}")
 
-  #   # apply the fact to hosts
-  #   apply_results = apply($supported_targets,
-  #                         '_description'  => 'Apply block: apply fact',
-  #                         '_catch_errors' => true,
-  #                         '_noop'         => $noop,) {
-  #     if $facts['os']['family'] == 'Windows' {
-  #       $path = 'C:\\ProgramData\\PuppetLabs\\facter\\facts.d\\'
-  #     } else {
-  #       $path = '/etc/facter/facts.d/'
-  #     }
+    # apply the fact to hosts
+    # apply_results = apply($supported_targets,
+    #                       '_description'  => 'Apply block: apply fact',
+    #                       '_catch_errors' => true,
+    #                       '_noop'         => $noop,) {
+    #   if $facts['os']['family'] == 'Windows' {
+    #     $path = 'C:\\ProgramData\\PuppetLabs\\facter\\facts.d\\'
+    #   } else {
+    #     $path = '/etc/facter/facts.d/'
+    #   }
 
-  #     file 'set fact': {
-  #       ensure  => present,
-  #       mode    => '0644',
-  #       path    => $path,
-  #       content => "${fact_name}=${fact_value}",
-  #     }
-  #   }
+    #   file 'set fact': {
+    #     ensure  => present,
+    #     mode    => '0644',
+    #     path    => $path,
+    #     content => "${fact_name}=${fact_value}",
+    #   }
+    # }
 
-  #   out::message("Apply results are ${apply_results}")
+    out::message("Apply results are ${apply_results}")
 
-  # }
-  # else {
-  #   fail('No valid nodes specified')
-  # }
+  }
+  else {
+    fail('No valid nodes specified')
+  }
 }
