08bafce6a282f40b60e78d4af33ada3307247702
Benjamin Robertson <ben.robertson@puppet.com>
2022-10-05 11:13:48 +1100

    removed PE test

--- site-modules/bolt/tasks/remove_ca_cert.sh
+++ site-modules/bolt/tasks/remove_ca_cert.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # check if we are running on a PE server
-which rpm
+# which rpm
 
-if [ $? -eq 0 ]
-then
-  rpm -qa | grep pe-puppetserver
-else
-  dpkg -l | grep pe-puppetserver
-fi
+# if [ $? -eq 0 ]
+# then
+#   rpm -qa | grep pe-puppetserver
+# else
+#   dpkg -l | grep pe-puppetserver
+# fi
 
-if [ $? -ne 0 ]
-then
-  echo "Bailing out: Running on PE server"
-  exit 1
-fi
+# if [ $? -ne 0 ]
+# then
+#   echo "Bailing out: Running on PE server"
+#   exit 1
+# fi
 
 # Remove ca cert
 rm /etc/puppetlabs/puppet/ssl/certs/ca.pem
