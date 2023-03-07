f49bb2e6f8f63e164dd98be4d594bebf232efaa9
ben <benrobertson9876@gmail.com>
2023-03-07 15:32:53 +1100

    cant get this right

--- manifests/site.pp
+++ manifests/site.pp
@@ -31,5 +31,5 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  $ben = 'set in site.pp test3123more change 2nd soe'
+  $ben = 'set in site.pp test3123more change 2nd test'
 }
