97ffac4305cca1349ebcf7b626b81fea86a71b7c
ben <benrobertson9876@gmail.com>
2023-03-07 15:17:26 +1100

    another commit

--- manifests/site.pp
+++ manifests/site.pp
@@ -31,5 +31,5 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  $ben = 'set in site.pp test12'
+  $ben = 'set in site.pp test13'
 }
