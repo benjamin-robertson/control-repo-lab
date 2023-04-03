0c2b065ee9fc02130c242862ebbc010e4acee168
ben <benrobertson9876@gmail.com>
2023-04-03 12:47:51 +1000

    Added random stuff

--- site-modules/profile/manifests/base_linux.pp
+++ site-modules/profile/manifests/base_linux.pp
@@ -5,4 +5,9 @@ class profile::base_linux {
   include profile::linux::motd_linux
   include profile::linux::files
   include soe_linux
+
+  file { '/tmp/random.txt':
+    ensure  => file,
+    content => 'so random',
+  }
 }
