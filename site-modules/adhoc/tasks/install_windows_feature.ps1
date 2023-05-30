eca148cc231d725f7a26ca167be6951c12ecae00
ben <benrobertson9876@gmail.com>
2023-05-30 18:08:31 +1000

    Added additional message

--- site-modules/adhoc/tasks/install_windows_feature.ps1
+++ site-modules/adhoc/tasks/install_windows_feature.ps1
@@ -4,6 +4,7 @@ $PT_windows_feature=$env:PT_windows_feature
 $feature_status=get-windowsfeature -name $PT_windows_feature
 
 echo "Installing windows feature ${PT_windows_feature}"
+echo "Current installation state"
 echo $feature_status
 
 if ($feature_status.InstallState -ne "Installed") {
