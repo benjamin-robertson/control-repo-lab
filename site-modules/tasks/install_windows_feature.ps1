b33eb9a72465de5d9fda03f775db08ba50ac16c1
ben <benrobertson9876@gmail.com>
2023-05-30 17:45:15 +1000

    Updates

--- site-modules/tasks/install_windows_feature.ps1
+++ site-modules/tasks/install_windows_feature.ps1
@@ -3,6 +3,9 @@ $PT_windows_feature=$env:PT_windows_feature
 
 $feature_status=get-windowsfeature -name $PT_windows_feature
 
+echo "Installing windows feature ${PT_windows_feature}"
+$echo $feature_status
+
 if ($feature_status.InstallState -ne "Installed") {
     Install-WindowsFeature -Name $PT_windows_feature
 }
