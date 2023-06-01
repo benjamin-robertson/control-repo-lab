12ee1dd29c95b49d745024f713b0a5cd4e7dbdb7
ben <benrobertson9876@gmail.com>
2023-06-01 17:32:42 +1000

    Remove fail

--- site-modules/profile/manifests/patching.pp
+++ site-modules/profile/manifests/patching.pp
@@ -11,14 +11,13 @@ class profile::patching {
   # notify { "patch groups are ${patch_groups}": }
   # notify { "result is ${result}": }
   # notify { "result length ${result.length}": }
-  notify { "group is ${result.keys['0']}": }
+  # notify { "group is ${result.keys['0']}": }
 
   if $result.length == 1 {
     class { 'pe_patch':
       patch_group => $result.keys['0'],
     }
   } else {
-    fail('No patch node group defined for host')
-    notify { "I have failed length is ${result.length}": }
+    notify { 'No patch group defined for host': }
   }
 }
