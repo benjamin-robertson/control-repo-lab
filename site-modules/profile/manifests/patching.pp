a18a078e2756e7877f6364888a23787989157852
ben <benrobertson9876@gmail.com>
2023-06-01 17:16:55 +1000

    Add notify with warning

--- site-modules/profile/manifests/patching.pp
+++ site-modules/profile/manifests/patching.pp
@@ -8,9 +8,9 @@ class profile::patching {
     $value.member($trusted['certname'])
   }
 
-  notify { "patch groups are ${patch_groups}": }
-  notify { "result is ${result}": }
-  notify { "result length ${result.length}": }
+  # notify { "patch groups are ${patch_groups}": }
+  # notify { "result is ${result}": }
+  # notify { "result length ${result.length}": }
   notify { "group is ${result.keys['0']}": }
 
   if $result.length == 1 {
@@ -19,5 +19,6 @@ class profile::patching {
     }
   } else {
     warning('No patch node group defined for host')
+    notify { "I have failed length is ${result.length}": }
   }
 }
