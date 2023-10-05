b7f3db36a87a93506e57a96d2658ce636228803e
ben <benrobertson9876@gmail.com>
2023-10-05 16:34:39 +1100

    chagne to nil

--- site-modules/profile/lib/facter/datacentre.rb
+++ site-modules/profile/lib/facter/datacentre.rb
@@ -1,9 +1,9 @@
 Facter.add(:datacentre) do
-    setcode do
-        if Facter.value(:trusted)['extensions']['pp_datacenter'] != undef
-          datacentre = Facter.value(:trusted)['extensions']['pp_datacenter'].downcase
-        else
-          'none-set'
-        end
+  setcode do
+    if Facter.value(:trusted)['extensions']['pp_datacenter'] != nil
+      datacentre = Facter.value(:trusted)['extensions']['pp_datacenter'].downcase
+    else
+      'none-set'
     end
   end
+end
