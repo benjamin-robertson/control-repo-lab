d29a5c4b12b60bbf7954990af61fe11d6f9620ab
ben <benrobertson9876@gmail.com>
2023-10-05 16:55:11 +1100

    test with trusted only

--- site-modules/profile/lib/facter/datacentre.rb
+++ site-modules/profile/lib/facter/datacentre.rb
@@ -4,14 +4,15 @@ def get_datacentre_regex(hostname)
 
 Facter.add(:datacentre) do
   setcode do
-    begin
-      if Facter.value(:trusted)['extensions']['pp_datacenter'] != nil
-       datacentre = Facter.value(:trusted)['extensions']['pp_datacenter'].downcase
-      else
-        get_datacentre_regex(Facter.value(:hostname).downcase)
-      end
-    rescue
-        get_datacentre_regex(Facter.value(:hostname).downcase)
-    end
+    datacentre = Facter.value(:trusted)['extensions']['pp_datacenter']
+    # begin
+    #   if Facter.value(:trusted)['extensions']['pp_datacenter'] != nil
+    #    datacentre = Facter.value(:trusted)['extensions']['pp_datacenter'].downcase
+    #   else
+    #     get_datacentre_regex(Facter.value(:hostname).downcase)
+    #   end
+    # rescue
+    #     get_datacentre_regex(Facter.value(:hostname).downcase)
+    # end
   end
 end
