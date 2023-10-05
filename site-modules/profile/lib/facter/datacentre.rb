4d9395f60e74b724a311bdf30858b2ac2ee04255
ben <benrobertson9876@gmail.com>
2023-10-05 17:04:46 +1100

    with new method

--- site-modules/profile/lib/facter/datacentre.rb
+++ site-modules/profile/lib/facter/datacentre.rb
@@ -1,18 +1,34 @@
+require 'puppet'
+require 'openssl'
+
 def get_datacentre_regex(hostname)
-    "none-set #{hostname}"
-  end
+  "none-set #{hostname}"
+end
 
 Facter.add(:datacentre) do
   setcode do
-    datacentre = Facter.value(:trusted)['extensions']['pp_datacenter']
-    # begin
-    #   if Facter.value(:trusted)['extensions']['pp_datacenter'] != nil
-    #    datacentre = Facter.value(:trusted)['extensions']['pp_datacenter'].downcase
-    #   else
-    #     get_datacentre_regex(Facter.value(:hostname).downcase)
-    #   end
-    # rescue
-    #     get_datacentre_regex(Facter.value(:hostname).downcase)
-    # end
+    # pick out the custom OID for pp_role, convert `:` to `_` and strip the leading `..` from the capture
+
+    def read_extention(extentions)
+        extentions.each do | element |
+            if element.oid == 'pp_datacenter' or element.oid == '1.3.6.1.4.1.34380.1.1.19'
+            arr = (element.to_s).split(' ')
+            result = arr[2,arr.length].join
+            return result
+            end
+        end
+        # element not found
+        return nil
+    end
+    
+    cert_data = File.open "#{Puppet.settings[:certdir]}/#{Puppet.settings[:certname].downcase}.pem"
+    
+    certificate = OpenSSL::X509::Certificate.new cert_data
+    result = read_extention(certificate.extensions)
+    if result == nil
+        datacenter = get_datacentre_regex(Facter.value(:hostname).downcase)
+    else
+        datacenter = result
+    end
   end
 end
