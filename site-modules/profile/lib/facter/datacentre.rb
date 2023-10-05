0b816e28fd2741d6a3a787db2c20a7b036034b7b
ben <benrobertson9876@gmail.com>
2023-10-05 17:09:05 +1100

    Fix output

--- site-modules/profile/lib/facter/datacentre.rb
+++ site-modules/profile/lib/facter/datacentre.rb
@@ -10,15 +10,15 @@ Facter.add(:datacentre) do
     # pick out the custom OID for pp_role, convert `:` to `_` and strip the leading `..` from the capture
 
     def read_extention(extentions)
-        extentions.each do | element |
-            if element.oid == 'pp_datacenter' or element.oid == '1.3.6.1.4.1.34380.1.1.19'
-            arr = (element.to_s).split(' ')
-            result = arr[2,arr.length].join
-            return result
-            end
+      extentions.each do | element |
+        if element.oid == 'pp_datacenter' or element.oid == '1.3.6.1.4.1.34380.1.1.19'
+          arr = (element.to_s).split(' ')
+          result = arr[2,arr.length].join
+        return result
         end
+      end
         # element not found
-        return nil
+      return nil
     end
     
     cert_data = File.open "#{Puppet.settings[:certdir]}/#{Puppet.settings[:certname].downcase}.pem"
@@ -26,9 +26,9 @@ Facter.add(:datacentre) do
     certificate = OpenSSL::X509::Certificate.new cert_data
     result = read_extention(certificate.extensions)
     if result == nil
-        datacenter = get_datacentre_regex(Facter.value(:hostname).downcase)
+      datacenter = get_datacentre_regex(Facter.value(:hostname).downcase)
     else
-        datacenter = result
+      datacenter = result.gsub(/[^a-zA-Z\d\/_-]/, '')
     end
   end
 end
