42d606cd8aafd4d57c377e8276b29eefa3acd8bb
ben <benrobertson9876@gmail.com>
2023-10-24 09:21:02 +1100

    Replace class name

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,7 +33,7 @@ node default {
 }
 
 node 'ip-172-31-18-43.ap-southeast-2.compute.internal' {
-  include profile::puppet::no_noop
+  include profile::puppet::enforced
   include role::generic_linux
 }
 
