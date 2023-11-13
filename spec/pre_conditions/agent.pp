2c92af6f56ff43e109803de77846aa2dc81799fc
ben <benrobertson9876@gmail.com>
2023-11-13 17:11:08 +1100

    remove class

--- spec/pre_conditions/agent.pp
+++ spec/pre_conditions/agent.pp
@@ -1,6 +1,6 @@
 # Class: puppet_enterprise::profile::agent
 # @summary mock test class
 #
-class puppet_enterprise::profile::agent {
-  # I do nothing
-}
+# class puppet_enterprise::profile::agent {
+#   # I do nothing
+# }
