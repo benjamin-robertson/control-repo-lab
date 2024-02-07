c52b1bf43d9ccfd4ebae5733e36a341f1e9e9f9c
ben <benrobertson9876@gmail.com>
2024-02-07 18:34:18 +1100

    Commit test

--- cd4pe_jobs/powershell_lint.ps1
+++ cd4pe_jobs/powershell_lint.ps1
@@ -1,3 +1,9 @@
-Invoke-Script   Analyzer -Recurse -Path /repo -ReportSummary > output.txt
-$tem p = Get-content output.txt -Raw
-Write-Ou tput $temp.Trim().Replace(" ", ".")
+pwsh --command Invoke-ScriptAnalyzer -Recurse -Path /repo -ReportSummary > output.txt
+$Scriptanalyzer_code = $LASTEXITCODE
+$temp = Get-content output.txt -Raw
+Write-Output $temp.Trim().Replace(" ", ".")
+if ( $Scriptanalyzer_code -eq 0  ) {
+    exit 0
+  } else {
+    exit 1
+}
