dca9e8bf967762355a9faf16ad1bf47dd08992f8
ben <benrobertson9876@gmail.com>
2024-02-07 18:43:44 +1100

    fix errors

--- cd4pe_jobs/powershell_lint.ps1
+++ cd4pe_jobs/powershell_lint.ps1
@@ -1,4 +1,4 @@
-pwsh -command Invoke-ScriptAnalyzer -Recurse -EnableExit -Path /repo -ReportSummary > output.txt 
+pwsh -command Invoke-ScriptAnalyzer -Recurse -EnableExit -Path /repo -ReportSummary > output.txt
 $Scriptanalyzer_code = $LASTEXITCODE
 $temp = Get-content output.txt -Raw
 Write-Output $temp.Trim().Replace(" ", ".")
