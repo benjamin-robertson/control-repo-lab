5105793ab1648a2686042dcbaecfaaac2ddcc138
ben <benrobertson9876@gmail.com>
2024-02-07 18:56:46 +1100

    new code

--- cd4pe_jobs/powershell_lint.ps1
+++ cd4pe_jobs/powershell_lint.ps1
@@ -1,7 +1,7 @@
 pwsh -command Invoke-ScriptAnalyzer -Recurse -EnableExit -Path /repo -ReportSummary > output.txt
 $Scriptanalyzer_code = $LASTEXITCODE
 $temp = Get-content output.txt -Raw
-Write-Output $temp.Trim().Replace(" ", ".")
+Write-Output $temp.Trim().Replace(" ", ".").Replace("[32;1m", "").Replace("[0m", "")
 if ( $Scriptanalyzer_code -eq 0  ) {
     exit 0
   } else {
