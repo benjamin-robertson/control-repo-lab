eef719652a58d5d18d43399800fe0598f8e26b8a
ben <benrobertson9876@gmail.com>
2024-03-14 11:21:43 +1100

    enable all rules

--- site-modules/role/manifests/acsc_appwhitelisting.pp
+++ site-modules/role/manifests/acsc_appwhitelisting.pp
@@ -3,6 +3,10 @@
 #
 class role::acsc_appwhitelisting {
   class { 'acsc_e8_application_control':
-    executable_rules => 'Enabled',
+    executable_rules   => 'Enabled',
+    msi_rules          => 'Enabled',
+    dll_rules          => 'Enabled',
+    script_rules       => 'Enabled',
+    packaged_app_rules => 'Enabled',
   }
 }
