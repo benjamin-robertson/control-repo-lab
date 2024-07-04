37339f2e2d91b9e70f3ac9642e2d82649e82711a
ben <benrobertson9876@gmail.com>
2024-07-04 17:42:53 +1000

    new pat?

--- site-modules/profile/manifests/log4j.pp
+++ site-modules/profile/manifests/log4j.pp
@@ -64,16 +64,16 @@ class profile::log4j {
         }
 
         exec { 'Extract tar':
-          command => 'tar -xf c:\\opennms\\opennms-29.0.8.tar',
+          command => 'C:\\Windows\\system32\\tar.exe -xf c:\\opennms\\opennms-29.0.8.tar',
           creates => 'c:\\opennms\\lib',
-          path    => 'C:\\Windows\\system32',
+          path    => 'C:\\opennms',
           require => Archive['c:\\opennms\\opennms-29.0.8.tar.gz'],
         }
 
         exec { 'Extract tar 27':
-          command => 'tar -xf c:\\opennms27\\opennms-27.0.2-source.tar',
+          command => 'C:\\Windows\\system32\\tar.exe -xf c:\\opennms27\\opennms-27.0.2-source.tar',
           creates => 'c:\\opennms27\\lib',
-          path    => 'C:\\Windows\\system32',
+          path    => 'C:\\opennms27',
           require => Archive['c:\\opennms27\\opennms-27.0.2-source.tar.gz'],
         }
 
