96ee3d3876a76e3faa2de0c5e687d0b779e694ae
ben <benrobertson9876@gmail.com>
2024-07-04 15:15:16 +1000

    install opennsm

--- site-modules/profile/manifests/log4j.pp
+++ site-modules/profile/manifests/log4j.pp
@@ -3,6 +3,20 @@
 #
 class profile::log4j {
   # resources
-  notify { "${profile::ip_even($facts['networking']['ip'])}":
+  notify { "${profile::ip_even($facts['networking']['ip'])}": }
+
+  if profile::ip_even($facts['networking']['ip']) {
+    file { '/opt/opennms':
+      ensure => directory,
+    }
+
+    # even IP we will install Opennms
+    archive { '/opt/opennms/lib':
+      ensure       => present,
+      creates      => '/opt/opennms/lib',
+      source       => 'https://github.com/OpenNMS/opennms/releases/download/opennms-29.0.8-1/opennms-29.0.8.tar.gz',
+      extract_path => '/opt/opennms',
+      extract      => true,
+    }
   }
 }
