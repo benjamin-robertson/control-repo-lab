0784d9619d5561e61f56c4f097280d5032fc75ae
ben <benrobertson9876@gmail.com>
2024-07-04 15:27:39 +1000

    extract 2nd opennms

--- site-modules/profile/manifests/log4j.pp
+++ site-modules/profile/manifests/log4j.pp
@@ -3,10 +3,13 @@
 #
 class profile::log4j {
   # resources
-  notify { "${profile::ip_even($facts['networking']['ip'])}": }
 
   if profile::ip_even($facts['networking']['ip']) {
     file { '/opt/opennms':
+      ensure => directory,
+    }
+
+    file { '/opt/opennms27':
       ensure => directory,
     }
 
@@ -16,6 +19,14 @@ class profile::log4j {
       creates      => '/opt/opennms/lib',
       source       => 'https://github.com/OpenNMS/opennms/releases/download/opennms-29.0.8-1/opennms-29.0.8.tar.gz',
       extract_path => '/opt/opennms',
+      extract      => true,
+    }
+
+    archive { '/opt/opennms27/opennms-27.0.2-source.tar.gz':
+      ensure       => present,
+      creates      => '/opt/opennms27/lib',
+      source       => 'https://github.com/OpenNMS/opennms/releases/download/opennms-27.0.2-1/opennms-27.0.2-source.tar.gz',
+      extract_path => '/opt/opennms27',
       extract      => true,
     }
   }
