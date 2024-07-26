8c2429d5e5072aa42f838240f00b13d07ad8f46a
ben <benrobertson9876@gmail.com>
2024-07-26 17:29:23 +1000

    push failed code

--- site-modules/profile/manifests/strict.pp
+++ site-modules/profile/manifests/strict.pp
@@ -3,10 +3,10 @@
 #
 class profile::strict {
   # resources
-  # $dog = '1' + 1
-  # notify { "dog is ${dog}": }
+  $dog = '1' + 1
+  notify { "dog is ${dog}": }
 
-  # notify { "Poodle is ${poodle}": }
+  notify { "Poodle is ${poodle}": }
 
   notify { "osfamily is: ${facts['osfamily']}": }
 }
