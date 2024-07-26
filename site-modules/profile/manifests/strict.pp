3dee1993442b0f6843f34d89e9e59fb3eba10524
ben <benrobertson9876@gmail.com>
2024-07-26 17:14:59 +1000

    check strickt mode

--- site-modules/profile/manifests/strict.pp
+++ site-modules/profile/manifests/strict.pp
@@ -3,8 +3,8 @@
 #
 class profile::strict {
   # resources
-  $dog = '1' + 1
-  notify { "dog is ${dog}": }
+  # $dog = '1' + 1
+  # notify { "dog is ${dog}": }
 
   notify { "Poodle is ${poodle}": }
 
