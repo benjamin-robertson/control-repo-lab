da977f707a4939e82ca1efd89a901609fc3961e2
ben <benrobertson9876@gmail.com>
2024-07-26 17:00:37 +1000

    push it

---
+++ site-modules/profile/manifests/strict.pp
@@ -0,0 +1,12 @@
+# Class: profile::strict
+#
+#
+class profile::strict {
+  # resources
+  $dog = '1' + 1
+  notify { "dog is ${dog}": }
+
+  notify { "Poodle is ${poodle}": }
+
+  notify { "osfamily is: ${facts['osfamily']}": }
+}
