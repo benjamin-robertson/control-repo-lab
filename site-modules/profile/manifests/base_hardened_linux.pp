e401b4743c1d347c41fef1e09f2e2ed208c284ff
ben <benrobertson9876@gmail.com>
2024-11-13 16:18:06 +1100

    with reload

--- site-modules/profile/manifests/base_hardened_linux.pp
+++ site-modules/profile/manifests/base_hardened_linux.pp
@@ -11,40 +11,9 @@ class profile::base_hardened_linux {
   include sce_linux
   # include ssh
   file_line { 'listen_address':
-    path  => '/etc/ssh/sshd_config',
-    line  => "ListenAddress ${facts['networking']['ip']}",
-    match => '^ListenAdddress ',
+    path   => '/etc/ssh/sshd_config',
+    line   => "ListenAddress ${facts['networking']['ip']}",
+    match  => '^ListenAdddress ',
+    notify => Exec['sce_reload_sshd'], # reload in the sce file
   }
-    #$timeservers = { 'server 1.1.1.1' => 'iburst minpoll 4 maxpoll 4'}
-    #Augeas {
-    #  context => '/files/etc/chrony.conf'
-    #}
-
-    #augeas { "chrony_d_agues":
-    #  changes => 'set server[1] 1.1.1.1',
-    #}
-
-    #augeas {"more stuff":
-    #  changes => 'touch server[1]/prefer'
-    #}
-
-    #augeas {"more s123tuff":
-    #  changes => 'touch server[1]/iburst'
-    #}
-
-    #augeas {"more s1df23tuff":
-    #  changes => 'touch server[1]/minpoll/=/4'
-    #}
-
-    #$_parts = split($timeservers[0], /\s+/)
-    #augeas { "chrony_${_parts[0]}_${_parts[1]}":
-    #  changes => "set ${_parts[0]}[1] ${_parts[1]}",
-    #}
-    #if size($_parts) > 2 {
-    #  $_parts[2, -1].each | String $prt | {
-    ##    augeas { "chrony_${_parts[0]}_${_parts[1]}_${prt}":
-    #     changes => "touch ${_parts[0]}[1]/${prt}",
-    #   }
-    # }
-  # }
 }
