1d6be7956dca64325317707e6788981ac35b47d3
ben <benrobertson9876@gmail.com>
2024-12-11 15:03:00 +1100

    create file

--- site-modules/profile/manifests/test_exporter.pp
+++ site-modules/profile/manifests/test_exporter.pp
@@ -5,6 +5,10 @@ class profile::test_exporter {
   # resources
   Host <<| tag == 'bens_hosts' |>>
 
+  file { '/tmp/my_hosts':
+    ensure => 'file',
+  }
+
   File_line <<| tag == 'test_line' |>>
 
   $query_results = puppetdb_query('resources { tag = "bens_hosts" }')
