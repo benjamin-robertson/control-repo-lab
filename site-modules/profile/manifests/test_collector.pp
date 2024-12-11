141f523455e836f95a678ae0f9d295642d32bbbf
ben <benrobertson9876@gmail.com>
2024-12-11 15:42:35 +1100

    commit with comments

--- site-modules/profile/manifests/test_collector.pp
+++ site-modules/profile/manifests/test_collector.pp
@@ -4,19 +4,17 @@
 class profile::test_collector {
   # Collect resources
 
+  # we are able to collect host entries. This can be done for any resource. 
+  #This will NOT create the host entry on this host this code is applied too. It will simply collect the data in PuppetDB.
   @@host { $facts['networking']['fqdn']:
     ip           => $facts['networking']['ip'],
     host_aliases => $facts['networking']['hostname'],
     tag          => 'bens_hosts',
   }
 
-  # collect all exported resources and realize them on this host
-  # Host <<||>>
-
-  # file { '/tmp/my_hosts':
-  #   ensure => 'file',
-  # }
-
+  # we are also able to collect defined types. This will not create a line in this file. Simply collect the data in PuppetDB. 
+  # We SHOULD tag all the resource we want to collect with a tag for use later. If we don't tag we run the risk
+  # of getting additional resources if they are also not tagged.
   @@file_line { "${facts['networking']['fqdn']}_line":
     ensure => 'present',
     path   => '/tmp/my_hosts',
--- site-modules/profile/manifests/test_exporter.pp
+++ site-modules/profile/manifests/test_exporter.pp
@@ -3,21 +3,28 @@
 #
 class profile::test_exporter {
   # resources
+
+  # This will create all the host entries which were collected in the 'test_collector' class with the tag bens_hosts
+  # and create them on the host this exporter class is applied too. 
   Host <<| tag == 'bens_hosts' |>>
 
   file { '/tmp/my_hosts':
     ensure => 'file',
   }
 
+  # This will populate the file '/tmp/my_hosts' with all the files lines were were collected from the 'test_collector' 
+  # class with the tag test_line.
   File_line <<| tag == 'test_line' |>>
 
+  # we can also simply query PuppetDB for exported resources with the tag 'bens_hosts'. This will provide an array containing 
+  # hashes of the exported resources. 1 hash per node/resource. 
   $query_results = puppetdb_query('resources { tag = "bens_hosts" }')
 
   $query_results.each | Hash $results | {
+    # We want to check if the resource is exported before printing. Otherwise will also get the resources which have been realised 
+    # on the target system.
     if $results['exported'] == true {
       notify { "Certname is ${results['certname']} IP is ${results['parameters']['ip']}": }
     }
   }
-
-  # notify { "${query_results}": }
 }
