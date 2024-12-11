531c494f56255da19408ba1b9e71908788b91c33
ben <benrobertson9876@gmail.com>
2024-12-11 15:00:22 +1100

    added exporter

--- site-modules/profile/manifests/test_exporter.pp
+++ site-modules/profile/manifests/test_exporter.pp
@@ -5,6 +5,8 @@ class profile::test_exporter {
   # resources
   Host <<| tag == 'bens_hosts' |>>
 
+  File_line <<| tag == 'test_line' |>>
+
   $query_results = puppetdb_query('resources { tag = "bens_hosts" }')
 
   notify { "${query_results}": }
