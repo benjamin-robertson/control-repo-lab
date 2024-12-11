c74e56565c52e42883751d673f7760a891ad8899
ben <benrobertson9876@gmail.com>
2024-12-11 15:48:25 +1100

    disable exporting hosts

--- site-modules/profile/manifests/test_exporter.pp
+++ site-modules/profile/manifests/test_exporter.pp
@@ -4,9 +4,9 @@
 class profile::test_exporter {
   # resources
 
-  # This will create all the host entries which were collected in the 'test_collector' class with the tag bens_hosts
-  # and create them on the host this exporter class is applied too. 
-  Host <<| tag == 'bens_hosts' |>>
+  # This will create all the host entries which were collected in the 'test_collector' class with the tag bens_hosts.
+  # Creating them on the host this exporter class is applied too. 
+  # Host <<| tag == 'bens_hosts' |>>
 
   file { '/tmp/my_hosts':
     ensure => 'file',
@@ -21,7 +21,7 @@ class profile::test_exporter {
   $query_results = puppetdb_query('resources { tag = "bens_hosts" }')
 
   $query_results.each | Hash $results | {
-    # We want to check if the resource is exported before printing. Otherwise will also get the resources which have been realised 
+    # We want to check if the resource is exported before printing. Otherwise will also get the resources which have been exported 
     # on the target system.
     if $results['exported'] == true {
       notify { "Certname is ${results['certname']} IP is ${results['parameters']['ip']}": }
