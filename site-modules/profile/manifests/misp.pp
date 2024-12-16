7f182e52170ad7e973ddfaee60f5c40648489a27
ben <benrobertson9876@gmail.com>
2024-12-16 17:22:43 +1100

    add own package source

--- site-modules/profile/manifests/misp.pp
+++ site-modules/profile/manifests/misp.pp
@@ -19,4 +19,15 @@ class profile::misp {
     source   => 'https://github.com/MISP/misp-docker.git',
     revision => '9bb03eab0ac1ac442e73820328033983dee1f1d4',
   }
+
+  apt::source { 'docker':
+    comment  => 'Docker',
+    location => 'https://download.docker.com/linux/ubuntu',
+    repos    => 'stable',
+    notify   => Exec['apt_update'],
+    key      => {
+      'name'   => 'docker-archive-keyring.asc',
+      'source' => 'https://download.docker.com/linux/ubuntu/gpg',
+    },
+  }
 }
