a9b78b7a5adfc584d6684122e9d3ac6ae87c19d7
ben <benrobertson9876@gmail.com>
2024-12-17 15:44:28 +1100

    Force vcsrepo

--- site-modules/profile/manifests/misp.pp
+++ site-modules/profile/manifests/misp.pp
@@ -25,6 +25,7 @@ class profile::misp (
     provider => git,
     source   => 'https://github.com/benjamin-robertson/misp-docker.git',
     revision => '46076a0f77a7348d5d505705243a44d93a26edad',
+    force    => true,
   }
 
   # setup config file
