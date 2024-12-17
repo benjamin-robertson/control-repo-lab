af7d4890b645a5de82e4d162210b829e6102cb71
ben <benrobertson9876@gmail.com>
2024-12-17 16:20:20 +1100

    Updaet commit

--- site-modules/profile/manifests/misp.pp
+++ site-modules/profile/manifests/misp.pp
@@ -24,7 +24,7 @@ class profile::misp (
     ensure   => present,
     provider => git,
     source   => 'https://github.com/benjamin-robertson/misp-docker.git',
-    revision => '46076a0f77a7348d5d505705243a44d93a26edad',
+    revision => '65eb813a8a37c455e4014da9bceddc5ec0c54324',
     excludes => [
       './configs',
       './files',
