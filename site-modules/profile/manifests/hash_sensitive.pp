f7bfcc64f05934c70f54167040d8edf86cf7c129
ben <benrobertson9876@gmail.com>
2025-01-06 09:22:53 +1100

    with lil hash

--- site-modules/profile/manifests/hash_sensitive.pp
+++ site-modules/profile/manifests/hash_sensitive.pp
@@ -1,11 +1,15 @@
 # Class: profile::hash_sensitive
 #
 # @param big_hash
+# @param lil_hash
 #
 class profile::hash_sensitive (
   Sensitive[Hash] $big_hash,
+  Hash $lil_hash,
 ) {
   # resources
-  notify { "Big has is: ${big_hash.unwrap}:":
+  notify { "Big has is: ${big_hash}:":
+  }
+  notify { "lil has is: ${lil_hash}:":
   }
 }
